42807f05e2cf423881e68a701c52c6ddfc42b11e
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2021-05-01 01:42:21 +0600

    Time: 96 ms (45.69%), Space: 48.2 MB (19.83%) - LeetHub

---
+++ container-with-most-water/container-with-most-water.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(heights) {
+  let p1 = 0, p2 = heights.length - 1, maxArea = 0;
+
+  while(p1 < p2) {
+    const height = Math.min(heights[p1], heights[p2]);
+    const width = p2 - p1;
+    const area = height * width;
+    maxArea = Math.max(maxArea, area);
+    
+    if(heights[p1] <= heights[p2]) {
+      p1++;
+    } else {
+      p2--;
+    }
+  }
+
+  return maxArea;
+}
