efe41ebd2990654d90c821448a33e8ac84401ad1
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-04-01 03:52:40 +0600

    Time: 118 ms (54.88%), Space: 42.6 MB (62.28%) - LeetHub

---
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var characterReplacement = function(s, k) {
+  let l = 0;
+  let count = {};
+  let maxLen = 0;
+  let maxF = 0;
+  
+  for (let r = 0; r < s.length; r++) {
+    if (!(s[r] in count)) {
+      count[s[r]] = 0;
+    }
+    count[s[r]]++;
+    
+    maxF = Math.max(maxF, count[s[r]]);
+    if (r - l + 1 - maxF > k) {
+      count[s[l]]--;
+      l++;
+    }
+    
+    maxLen = Math.max(r - l + 1, maxLen);
+  }
+  
+  return maxLen;
+    
+};
