49672ce9aca0727efd8f07a6216daf50f0f2caa4
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-04-08 02:01:00 +0600

    Time: 113 ms (55.61%), Space: 45.9 MB (87.60%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.js
@@ -0,0 +1,42 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+var canFinish = function(numCourses, prerequisites) {
+  let preMap = {};
+  for (let i = 0; i < numCourses; i++) {
+    preMap[i] = [];
+  }
+
+  for (let course of prerequisites) {
+    preMap[course[0]].push(course[1]);
+  }
+  
+  const visited = new Set();
+  
+  function dfs(course) {
+    if (visited.has(course)) return false;
+    if (preMap[course].length === 0) return true;
+    
+    visited.add(course);
+    for (let pre of preMap[course]) {
+      if (!dfs(pre)) return false;
+    }
+    visited.delete(course);
+    preMap[course] = [];
+    return true;
+  }
+  
+  
+  
+  for (let i = 0; i < numCourses; i++) {
+    if (!dfs(i)) {
+      return false;
+    }
+  }
+  
+  return true;
+  
+  
+};
