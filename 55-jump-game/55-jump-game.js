977a41eac30ebe6d9102ff2c84f97cb10a14d54f
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-04-19 21:50:52 +0600

    Time: 75 ms (90.93%), Space: 46.1 MB (85.52%) - LeetHub

---
+++ 55-jump-game/55-jump-game.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+  let target = nums.length - 1;
+  
+  for (let i = nums.length -1; i >= 0; i--) {
+    if (i + nums[i] >= target) {
+      target = i;
+    }
+  }
+  
+  return target === 0 ? true : false;
+    
+};
