08c9c449ad40b5a40058428082d2a68f2f49d904
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-04-21 20:06:04 +0600

    Time: 79 ms (67.87%), Space: 44.7 MB (14.87%) - LeetHub

---
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(list1, list2) {
+  let resultHead = new ListNode();
+  let tail = resultHead;
+  
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      tail.next = list1;
+      list1 = list1.next;
+    } else {
+      tail.next = list2;
+      list2 = list2.next;
+    }
+    tail = tail.next;
+  }
+  
+  tail.next = list1 || list2;
+  return resultHead.next;
+    
+};
