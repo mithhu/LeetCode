97d924e94ed2bd9cb30d3efb62786363935e784d
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-04-21 20:53:26 +0600

    Time: 87 ms (66.04%), Space: 47.1 MB (34.41%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -0,0 +1,38 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+  let l = 0, r = s.length - 1;
+  while (l < r) {
+    while(l < r &&!isAlphaNumeric(s[l])) {
+      l++;
+    }
+    while(l < r &&!isAlphaNumeric(s[r])) {
+      r--;
+    }
+    
+    if (s[l].toLowerCase() !== s[r].toLowerCase()) {
+      return false;
+    }
+    l++;
+    r--;
+  }
+  
+  return true;
+    
+};
+
+
+
+function isAlphaNumeric(ch) {
+  let code = ch.charCodeAt(); //48-58 65 - 90 97 - 122
+  
+  if (("A".charCodeAt() <= code && "Z".charCodeAt() >= code) ||
+      ("a".charCodeAt() <= code && "z".charCodeAt() >= code) ||
+      ("0".charCodeAt() <= code && "9".charCodeAt() >= code)) {    
+    return true;
+  }
+  return false;
+  
+}
