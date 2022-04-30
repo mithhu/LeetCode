6d0bcf32887c9a8d23479b6e294ceef7f4509e0b
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-05-01 02:26:37 +0600

    Time: 143 ms (41.28%), Space: 71.2 MB (15.37%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.js
+++ 518-coin-change-2/518-coin-change-2.js
@@ -8,20 +8,9 @@ var change = function(amount, coins) {
   let rows = coins.length + 1;
   
   let dp = Array(rows).fill().map(() => Array(cols).fill(0));
-
-  // console.log(dp)
-  
-  // for (let i = 0; i < cols; i++) {
-  //   dp[0][i] = 0;
-  // }
   for (let i = 0; i < rows; i++) {
     dp[i][0] = 1;
   }
-
-  console.log(dp)
-  
-  
-  
   for (let r = 1; r < rows; r++) {
     for (let c = 1; c < cols; c++) {
       if (coins[r - 1] <= c) {
