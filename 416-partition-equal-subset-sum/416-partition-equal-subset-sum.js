0b0228236e4e71a26e591a9ca39cb3ae674cbb10
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-05-01 23:39:46 +0600

    Time: 320 ms (44.12%), Space: 89.7 MB (16.78%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.js
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.js
@@ -5,25 +5,29 @@
 var canPartition = function(nums) {
   const numsVal = nums.reduce((sum, curr) => curr + sum, 0)
   if (numsVal % 2) return false;
-  
-  let dp = new Set();
-  dp.add(0);
-  
   let target = numsVal / 2;
   
-  for (let i = 0; i < nums.length; i++) {
-    let nextDp = new Set();
-    for (let t of dp) {
-      if (t + nums[i] === target) {
-        return true;
-      }
-      nextDp.add(t + nums[i]);
-      nextDp.add(t);
-      
-    }
-    dp = nextDp;
+  console.log(target)
+  
+  let cols = target + 1;
+  let rows = nums.length + 1;
+  
+  let dp = Array(rows).fill().map(() => Array(cols).fill(false));
+  
+  for (let r = 0; r < rows; r++) {
+    dp[r][0] = true;
   }
   
-  return dp.has(target)
-    
+  for (let r = 1; r < rows; r++) {
+    for (let c = 1; c < cols; c++) {
+      if (nums[r - 1] <= c) {
+        dp[r][c] = dp[r - 1][c] || dp[r - 1][c - nums[r - 1]]
+      } else {
+        dp[r][c] = dp[r - 1][c] ;
+      }
+    }
+  }
+  
+  return dp[dp.length - 1][dp[0].length - 1]
+  
 };
