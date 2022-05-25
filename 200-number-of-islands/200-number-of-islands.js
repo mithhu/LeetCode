c7556b04ab698b26a179b4cc1a5bcf6ef8931e25
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-05-25 22:34:23 +0600

    Time: 107 ms (62.01%), Space: 45 MB (65.26%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.js
+++ 200-number-of-islands/200-number-of-islands.js
@@ -2,48 +2,26 @@
  * @param {character[][]} grid
  * @return {number}
  */
-
-
-
-
 var numIslands = function(grid) {
   if (grid.length === 0) return 0;
-
-  const directions = [
-    [0, 1],
-    [0, -1],
-    [1, 0],
-    [-1, 0],  
-  ]
-  
+  let ROWS = grid.length;
+  let COLS = grid[0].length;
   let count = 0;
   
-  for (let i = 0; i < grid.length; i++) {
-    for (let j = 0; j < grid[0].length; j++) {
-      if (grid[i][j] === "1") {
+  function dfs(r, c) {
+    if (r < 0 || r >= ROWS || c < 0 || c >= COLS || grid[r][c] === "0") return;
+    grid[r][c] = "0";
+    dfs(r + 1, c);
+    dfs(r - 1, c);
+    dfs(r, c + 1);
+    dfs(r, c - 1);
+  }
+  
+  for (let r = 0; r < ROWS; r++) {
+    for (let c = 0; c < COLS; c++) {
+      if (grid[r][c] === "1") {
         count++;
-        const q = new Queue();
-        q.enqueue([i, j]);
-        grid[i][j] = "0";
-        
-        while (q.size() > 0) {
-          const [cR, cC] = q.dequeue();
-          for (let d = 0; d < directions.length; d++) {
-            let currentD = directions[d];
-            let nextR = cR + currentD[0];
-            let nextC = cC + currentD[1];
-            
-            if (nextR < 0 || nextR > grid.length - 1 || nextC < 0 || nextC > grid[0].length - 1) {
-              continue;
-            }
-
-            if (grid[nextR][nextC] === "1") {
-              q.enqueue([nextR, nextC]);
-              grid[nextR][nextC] = "0";
-            }  
-          }
-          
-        }
+        dfs(r, c);
       }
     }
   }
