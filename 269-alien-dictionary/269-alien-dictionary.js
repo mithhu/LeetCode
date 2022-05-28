8762a8e0e731b0af8705c21a8ddbb834dc7b4402
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-05-29 01:31:02 +0600

    Time: 86 ms (78.16%), Space: 46.3 MB (37.14%) - LeetHub

--- 269-alien-dictionary/269-alien-dictionary.js
+++ 269-alien-dictionary/269-alien-dictionary.js
@@ -6,7 +6,7 @@ var alienOrder = function(words) {
   let adj = {};
   for (let w of words) {
     for (let c of w) {
-      adj[c] = new Set();
+      adj[c] = new Set()
     }
   }
   
@@ -15,10 +15,9 @@ var alienOrder = function(words) {
     let w2 = words[i + 1];
     let minLen = Math.min(w1.length, w2.length);
     
-    if (w1.length > w2.length && w1.slice(0, minLen) === w2.slice(0, minLen)) {
+    if (w1.length > w2.length && w1.substring(0, minLen) === w2.substring(0, minLen)) {
       return "";
     }
-    
     for (let j = 0; j < minLen; j++) {
       if (w1[j] !== w2[j]) {
         adj[w1[j]].add(w2[j]);
@@ -26,7 +25,7 @@ var alienOrder = function(words) {
       }
     }
   }
-
+  
   let visit = {};
   let res = [];
   
@@ -34,6 +33,7 @@ var alienOrder = function(words) {
     if (c in visit) {
       return visit[c];
     }
+    
     visit[c] = true;
     for (let nei of adj[c]) {
       if (dfs(nei)) {
@@ -43,12 +43,13 @@ var alienOrder = function(words) {
     visit[c] = false;
     res.push(c);
   }
-    
+  
   for (let c in adj) {
     if (dfs(c)) {
       return "";
     }
   }
-  return res.reverse().join("");
   
+  return res.reverse().join("");
+    
 };
