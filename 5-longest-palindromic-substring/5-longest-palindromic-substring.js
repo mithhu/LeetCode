2ad2eb2b32f18b85f5c25a209cf591b9ac2ff34d
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-05-31 20:36:07 +0600

    Time: 144 ms (66.93%), Space: 45.1 MB (74.02%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.js
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.js
@@ -3,35 +3,40 @@
  * @return {string}
  */
 var longestPalindrome = function(s) {
+  //init
   let res = "";
   let resLen = 0;
   
   for (let i = 0; i < s.length; i++) {
-    let l = i, r = i;
-  
-    while (l >= 0 && r <= s.length - 1 && s[l] === s[r]) {
-      if (r - l + 1 > resLen) {
-        res = s.slice(l, r + 1);
-        resLen = r - l + 1
-      }
-      l--;
-      r++;
-    }
+    //odd
+    let l = i;
+    let r = i;
     
-    l = i, r = i + 1;
-  
-    while (l >= 0 && r <= s.length - 1 && s[l] === s[r]) {
+    while (l >= 0 && r < s.length && s[l] === s[r]) {
       if (r - l + 1 > resLen) {
+        resLen = r - l + 1;
         res = s.slice(l, r + 1);
-        resLen = r - l + 1
       }
       l--;
       r++;
     }
+
+    //even
+    l = i;
+    r = i + 1;
+    
+    while (l >= 0 && r < s.length && s[l] === s[r]) {
+      if (r - l + 1 > resLen) {
+        resLen = r - l + 1;
+        res = s.slice(l, r + 1);
+      }
+      l--;
+      r++;
+    }
+      
   }
   
   return res;
-  
-  
     
 };
+
