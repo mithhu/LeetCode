6589ca3418eddc1a8a94d792076e38a55f0e1a07
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-01 03:12:23 +0600

    Time: 118 ms (23.62%), Space: 42.8 MB (48.46%) - LeetHub

--- 91-decode-ways/91-decode-ways.js
+++ 91-decode-ways/91-decode-ways.js
@@ -4,23 +4,22 @@
  */
 var numDecodings = function(s) {
   let dp = Array(s.length + 1).fill(0);
-  dp[dp.length - 1] = 1;
+  dp[0] = 1;
   
-  for (let i = s.length - 1; i >= 0; i--) {
-    if (s[i] === "0") {
+  for (let i = 1; i < s.length + 1; i++) {
+    if (s[i - 1] === "0") {
       dp[i] = 0;
     } else {
-      dp[i] = dp[i + 1];
+      dp[i] = dp[i - 1];
     }
     
-    if (i + 1 < s.length && (s[i] === "1" ||
-       s[i] === "2" && "0123456".split("").includes(s[i + 1]))
+    if (i - 2 >= 0  && (s[i - 2] === "1" ||
+       s[i - 2] === "2" && "0123456".split("").includes(s[i - 1]))
        ) {
-      dp[i] += dp[i + 2];
+      dp[i] += dp[i - 2];
     }
   }
-
-  return dp[0];
+  return dp[dp.length - 1];
   
     
 };
