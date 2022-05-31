6f1099b88c54e7b3f961769c39b54cd5fbc3b6ef
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-01 04:36:47 +0600

    Time: 275 ms (5.04%), Space: 50.7 MB (5.04%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.js
+++ 152-maximum-product-subarray/152-maximum-product-subarray.js
@@ -4,20 +4,16 @@
  */
 
 var maxProduct = function(nums) {
-  let res = Math.max(...nums);
+  let res = -Infinity;
   let curMin = 1;
   let curMax = 1;
   
   for (let n of nums) {
-    // if (n === 0) {
-    //   curMin = 1;
-    //   curMax = 1;
-    //   continue;
-    // }
     let temp = curMax * n;
     curMax = Math.max(n, curMax * n, curMin * n);
     curMin = Math.min(n, temp, curMin * n);
     res = Math.max(res, curMax);
+    console.log(curMax, curMin, res)
   }
   
   return res;
