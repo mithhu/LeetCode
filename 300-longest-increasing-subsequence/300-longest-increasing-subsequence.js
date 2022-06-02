35579cffbe989752e7ac7fc2a001c11f85a01f7f
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-03 03:06:31 +0600

    Time: 179 ms (66.68%), Space: 43.7 MB (67.62%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.js
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.js
@@ -3,16 +3,16 @@
  * @return {number}
  */
 var lengthOfLIS = function(nums) {
-  const LIS = Array(nums.length).fill(1);
+  let LIS = Array(nums.length).fill(1);
   
-  for (let i = nums.length - 1; i >= 0; i--) {
+  for (let i = nums.length - 2; i >= 0; i--) {
     for (let j = i + 1; j < nums.length; j++) {
       if (nums[i] < nums[j]) {
-        LIS[i] = Math.max(LIS[i], 1 + LIS[j]);        
+        LIS[i] = Math.max(LIS[i], 1 + LIS[j]);
       }
     }
   }
-
+  
   return Math.max(...LIS);
     
 };
