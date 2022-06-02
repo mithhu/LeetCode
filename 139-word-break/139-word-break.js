0414666de12251e23a8897309995e392f0d02108
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-03 01:42:18 +0600

    Time: 87 ms (56.36%), Space: 43.7 MB (71.58%) - LeetHub

--- 139-word-break/139-word-break.js
+++ 139-word-break/139-word-break.js
@@ -7,16 +7,19 @@ var wordBreak = function(s, wordDict) {
   const dp = Array(s.length + 1).fill(false);
   dp[s.length] = true;
   
+  let words = new Set(wordDict);
+  
   for (let i = s.length - 1; i >= 0; i--) {
-    for (let word of wordDict) {
-      if (i + word.length <= s.length && word === s.slice(i, i + word.length)) {
+    for (let word of words) {
+      if (i + word.length <= s.length && s.substring(i, i + word.length) === word) {
         dp[i] = dp[i + word.length];
-      } 
+      }
       if (dp[i]) {
         break;
       }
     }
   }
+  
   return dp[0];
     
 };
