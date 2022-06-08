e612064abe62461b40c1bc7031f6788966ca0184
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-09 01:07:18 +0600

    Time: 124 ms (10.37%), Space: 42 MB (61.41%) - LeetHub

---
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var checkValidString = function(s) {
+  let leftMin = 0;
+  let leftMax = 0;
+  
+  for (let ch of s) {
+    if (ch === "(") {
+      leftMin++;
+      leftMax++;
+    } else if (ch === ")") {
+      leftMin--;
+      leftMax--;
+    } else { // *
+      leftMin--; //)
+      leftMax++; //(
+    }
+    
+    if (leftMax < 0) {
+      return false;
+    }
+    
+    if (leftMin < 0) { // "" )
+      leftMin = 0;
+    }
+  }
+   
+  return leftMin === 0;
+};
