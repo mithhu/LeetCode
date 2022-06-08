134140a097f8796f5f9d295a53e0e6a1127058fd
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-09 04:09:19 +0600

    Time: 113 ms (25.64%), Space: 44.1 MB (68.21%) - LeetHub

--- 252-meeting-rooms/252-meeting-rooms.js
+++ 252-meeting-rooms/252-meeting-rooms.js
@@ -6,10 +6,12 @@ var canAttendMeetings = function(intervals) {
   intervals.sort((a, b) => a[0] - b[0]);
   
   for (let i = 1; i < intervals.length; i++) {
-    let s = intervals[i][0];
-    let e = intervals[i - 1][1];
+    let start = intervals[i][0];
+    let prevEnd = intervals[i - 1][1];
     
-    if (e > s) return false;   
-  }  
-  return true;  
+    if (prevEnd > start) return false;
+  }
+  
+  return true;
+    
 };
