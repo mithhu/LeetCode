7fb79d698d6a87f213d05747ab6907f04d7e57f3
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-10 02:01:34 +0600

    Time: 599 ms (84.38%), Space: 109.6 MB (75.00%) - LeetHub

---
+++ 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} queries
+ * @return {number[]}
+ */
+var minInterval = function(intervals, queries) {
+  intervals.sort((a, b) => {
+    if (a[0] !== b[0]) {
+      return a[0] - b[0]
+    }
+    return a[1] - b[1];
+  });
+  let res = {};
+  let i = 0;
+  let minHeap = new MinPriorityQueue({compare: (a, b) => a[0] - b[0]});
+  
+  
+  
+  for (let q  of [...queries].sort((a, b) => a - b)) {
+    while (i < intervals.length && intervals[i][0] <= q) {
+      let [l, r] = intervals[i];
+      minHeap.enqueue([r - l + 1, r]);
+      i++;
+    }
+    
+    while (minHeap.size() && minHeap.front()[1] < q) {
+      minHeap.dequeue();
+    }
+    res[q] = minHeap.size() ? minHeap.front()[0] : -1;
+  }
+  
+  let result = [];
+  
+  for (let q of queries) {
+    result.push(res[q]);
+  }
+  
+  return result;
+};
