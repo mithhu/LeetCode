3ba8e1a66ee2dfe5b201e10fb6b12f8e709b585a
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-10 20:27:02 +0600

    Time: 118 ms (28.19%), Space: 45.6 MB (23.77%) - LeetHub

---
+++ 46-permutations/46-permutations.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+  let result = [];
+  
+  if (nums.length === 1) {
+    return [[...nums]];
+  }
+  
+  for (let i = 0; i < nums.length; i++) {
+    let n = nums.shift();
+    let perms = permute(nums);
+
+    if (perms.length) {
+      for (let perm of perms) {
+        perm.push(n);
+      }
+    }
+    result.push(...perms);
+    nums.push(n);
+  }
+  
+  return result;
+  
+};
