a4272d66652d3b5582c0f463d6a8c7a0001c1e16
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-11 17:48:56 +0600

    Time: 94 ms (24.98%), Space: 42.5 MB (39.36%) - LeetHub

--- 100-same-tree/100-same-tree.js
+++ 100-same-tree/100-same-tree.js
@@ -12,9 +12,13 @@
  * @return {boolean}
  */
 var isSameTree = function(p, q) {
-  if (!p && !q) return true;
-  if (!p || !q || p.val !== q.val) return false;
-  
-  return (isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
-    
+  if (!p && !q) {
+    return true;
+  }    
+  if (!p || !q || p.val !== q.val) {
+    return false;
+  }
+
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+
 };
