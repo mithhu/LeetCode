a41c6fffb42445e03ca071ce9babe72e32d1d7df
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-12 05:08:22 +0600

    Time: 2596 ms (59.09%), Space: 78 MB (86.88%) - LeetHub

--- 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.js
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.js
@@ -1,49 +1,51 @@
 class TrieNode {
   constructor() {
-    this.children = {};
-    this.end = false;
+    this.childrens = {};
+    this.endW = false;
   }
 }
+
 
 class WordDictionary {
   constructor() {
-    this.root = new TrieNode(); // {b: {a: }, end : false}, end: false
+    this.root = new TrieNode();
   }
-  
   addWord(word) {
     let curr = this.root;
+    
     for (let ch of word) {
-      if (!(ch in curr.children)) {
-        curr.children[ch] = new TrieNode();
+      if (!(ch in curr.childrens)) {
+        curr.childrens[ch] = new TrieNode();
       }
-      curr = curr.children[ch];
+      curr = curr.childrens[ch];
     }
-    curr.end = true;
+    curr.endW = true;
   }
   
   search(word) {
-    function dfs(j, root) {
-      let curr = root;
-      for (let i = j; i < word.length; i++) {
-          let ch = word[i];
-          if (ch === ".") {
-            for (let child of Object.values(curr.children)) {
+    function dfs(idx, node) {
+      let curr = node;
+
+      for (let i = idx; i < word.length; i++) {
+        let ch = word[i];
+        if (ch === ".") {
+          for (let child of Object.values(curr.childrens)) {
               if (dfs(i + 1, child)) {
                 return true;
               }
-            }
+          }
+          return false;
+        } else {
+          if (!(ch in curr.childrens)) {
             return false;
-
-          } else {
-            if (!(ch in curr.children)) {
-              return false;
-            } else {
-              curr = curr.children[ch];
-            }
-          }  
+          }
+          curr = curr.childrens[ch];
         }
-      return curr.end;
+      }
+
+
+      return curr.endW;
     }
     return dfs(0, this.root);
   }
-}
+};
