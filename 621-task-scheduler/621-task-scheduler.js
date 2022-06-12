b17ef3e9c19aef5deb08fddca9b4857ef4d43e1c
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-13 02:47:45 +0600

    Time: 543 ms (5.11%), Space: 56.3 MB (11.00%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.js
+++ 621-task-scheduler/621-task-scheduler.js
@@ -11,19 +11,19 @@ var leastInterval = function(tasks, n) {
   }
   
   let time = 0;
-  let q = [];
+  let q = new Queue();
   
-  while (maxHeap.size() || q.length) {
+  while (maxHeap.size() || q.size()) {
     time++;
     if (maxHeap.size()) {
       let cnt = maxHeap.dequeue().element - 1;
       if (cnt) {
-        q.push([cnt, time + n])        
+        q.enqueue([cnt, time + n])        
       }
     }
     
-    if (q.length && q[0][1] === time) {
-      maxHeap.enqueue([q.shift()[0]])
+    if (q.size() && q.front()[1] === time) {
+      maxHeap.enqueue([q.dequeue()[0]])
     }
   }
   
