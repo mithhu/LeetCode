d090661507c9e8acdf2da233b7617630341f2cac
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-13 12:41:11 +0600

    Time: 996 ms (52.09%), Space: 96.7 MB (41.67%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.js
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.js
@@ -5,44 +5,29 @@ class MedianFinder {
     this.large = new MinPriorityQueue();
   }
   
+  
   addNum(num) {
     if (this.small.isEmpty() || this.small.front().element >= num) {
       this.small.enqueue(num);
     } else {
       this.large.enqueue(num);
-    }
+    }  
     
     if (this.small.size() > this.large.size() + 1) {
       let val = this.small.dequeue().element;
       this.large.enqueue(val);
-    } else if (this.large.size() > this.small.size()) {
+    } else if (this.small.size() < this.large.size()) {
       let val = this.large.dequeue().element;
       this.small.enqueue(val);
     }
-  };
+  }
   
   findMedian() {
+    //2 bucket equal
     if (this.small.size() === this.large.size()) {
-      return (this.small.front().element + this.large.front().element) / 2
+      return (this.small.front().element + this.large.front().element) / 2;
     }
-    
     return this.small.front().element;
   }
-}
-/** 
- * @param {number} num
- * @return {void}
- */
-
-
-/**
- * @return {number}
- */
-
-
-/** 
- * Your MedianFinder object will be instantiated and called as such:
- * var obj = new MedianFinder()
- * obj.addNum(num)
- * var param_2 = obj.findMedian()
- */
+    
+};
