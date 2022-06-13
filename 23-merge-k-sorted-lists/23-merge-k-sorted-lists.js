b1d429ae5662b52ff230d23c775322df49db22fd
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-14 04:36:48 +0600

    Time: 102 ms (88.15%), Space: 47.2 MB (72.00%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.js
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.js
@@ -10,7 +10,9 @@
  * @return {ListNode}
  */
 var mergeKLists = function(lists) {
-  if (lists === null || lists.length === 0) return null;
+  if (lists === null || lists.length === 0) {
+    return null;
+  }
   
   while (lists.length > 1) {
     let mergedLists = [];
@@ -18,34 +20,29 @@ var mergeKLists = function(lists) {
     for (let i = 0; i < lists.length; i += 2) {
       let l1 = lists[i];
       let l2 = i + 1 < lists.length ? lists[i + 1] : null;
-      mergedLists.push(mergedList(l1, l2));
+      mergedLists.push(mergeList(l1, l2))
     }
     lists = mergedLists;
   }
+  
   return lists[0];
-    
 };
 
-function mergedList(l1, l2) {
+function mergeList(l1, l2) {
   let dummy = new ListNode();
   let tail = dummy;
+  
   while (l1 && l2) {
     if (l1.val < l2.val) {
       tail.next = l1;
       l1 = l1.next;
     } else {
-      
       tail.next = l2;
       l2 = l2.next;
     }
     tail = tail.next;
   }
   
-  if (l1) {
-    tail.next = l1;
-  }
-  if (l2) {
-    tail.next = l2;
-  }
+  tail.next = l1 || l2;
   return dummy.next;
 }
