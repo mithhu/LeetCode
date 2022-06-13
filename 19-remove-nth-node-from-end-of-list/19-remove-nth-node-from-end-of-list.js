84c040b32d57efc757a87f2071f19ce30e75dd05
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-13 18:46:48 +0600

    Time: 107 ms (19.40%), Space: 42.5 MB (73.23%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.js
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.js
@@ -12,20 +12,23 @@
  */
 var removeNthFromEnd = function(head, n) {
   let dummy = new ListNode(0, head);
+  
   let left = dummy;
   let right = head;
   
-  while (n > 0 && right) {
+  while (right && n > 0) {
     right = right.next;
-    n--;
+    n -= 1;
   }
   
-  while (right) {
+  while(right) {
     right = right.next;
     left = left.next;
   }
   
   left.next = left.next.next;
+  
   return dummy.next;
   
+    
 };
