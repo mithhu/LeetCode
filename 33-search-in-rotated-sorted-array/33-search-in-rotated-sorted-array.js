5f7f73002e3872415a1b8c85472735e4959e52cd
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-15 03:52:21 +0600

    Time: 109 ms (13.42%), Space: 42.5 MB (16.42%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+  let l = 0;
+  let r = nums.length - 1;
+  
+  while (l <= r) {
+    let mid = l + Math.floor((r - l) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    }
+    
+    //left sorted
+    if (nums[l] <= nums[mid]) {
+      if (nums[l] > target || target > nums[mid]) {
+        l = mid + 1;
+      } else {
+        r = mid - 1;
+      }
+    } else {
+    //right sorted
+      if (target > nums[r] || target < nums[mid]) {
+        r = mid - 1;
+      } else {
+        l = mid + 1;
+      }
+    }
+    
+  }
+  
+  return -1;
+    
+};
