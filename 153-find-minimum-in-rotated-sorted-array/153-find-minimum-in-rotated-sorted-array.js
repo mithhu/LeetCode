431fd11523b75e144044407ea784610fbfb921de
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-15 04:20:53 +0600

    Time: 68 ms (78.37%), Space: 42.1 MB (60.40%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.js
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.js
@@ -4,20 +4,25 @@
  */
 var findMin = function(nums) {
   let l = 0;
-  let r = nums.length - 1;
-  let right = nums[nums.length - 1];
+  let r = nums.length -1;
+  
+  let res = nums[0];
   
   while (l <= r) {
-    const mid = Math.floor(l + (r - l) / 2);
-    if (nums[mid] <= right) {
-      if (mid === 0 || nums[mid - 1] > nums[mid]) {
-        return nums[mid];
-      }
-      r = mid - 1;
-    } else {
+    if (nums[l] < nums[r]) {
+      res = Math.min(nums[l], res);
+      break;
+    }
+    
+    let mid = l + Math.floor((r - l) / 2);
+    res = Math.min(res, nums[mid]);
+    if (nums[l] <= nums[mid]) {
       l = mid + 1;
+    } else {
+      r = mid - 1
     }
   }
   
-  return -1
-}
+  return res;
+    
+};
