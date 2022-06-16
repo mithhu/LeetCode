a3b569eee89d17a0643b8643c4cd07fed3bf9f22
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-16 13:55:21 +0600

    Time: 91 ms (81.63%), Space: 42.8 MB (86.26%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+  if (s.length !== t.length) return false;
+  
+  let map = {};
+  
+  for (let i = 0; i < s.length; i++) {
+    if (!(s[i] in map)) {
+      map[s[i]] = 0;
+    }
+    map[s[i]]++;
+    if (!(t[i] in map)) {
+      map[t[i]] = 0;
+    }
+    map[t[i]]--;
+  }
+
+  console.log(map)
+  
+  for (let key in map) {
+    if (map[key] !== 0) {
+      return false;
+    }
+  }
+  
+  return true;
+    
+};
