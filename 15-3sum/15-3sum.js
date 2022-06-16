11f6bf70d79f63e738b48368f04759bd74d32e74
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-17 03:48:39 +0600

    Time: 168 ms (79.21%), Space: 53.3 MB (32.33%) - LeetHub

---
+++ 15-3sum/15-3sum.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+  let triplets = [];
+  nums.sort((a, b) => a - b);
+  
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) {
+      continue;
+    }
+    let l = i + 1;
+    let r = nums.length - 1;
+    
+    while (l < r) {
+      let sum = nums[i] + nums[l] + nums[r];
+      if (sum > 0) {
+        r--;
+      } else if (sum < 0) {
+        l++
+      } else {
+        triplets.push([nums[i], nums[l], nums[r]]);
+        l++;
+        while (l < r && nums[l] === nums[l - 1]) {
+          l++;
+        }
+      }
+    }
+  }
+  
+  return triplets;
+    
+};
