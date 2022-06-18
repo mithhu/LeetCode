2c5055a0fe5f8f4f05e191b9489a3a8b2ad637fe
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-19 04:51:39 +0600

    Time: 121 ms (38.23%), Space: 46.9 MB (48.98%) - LeetHub

--- 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.js
+++ 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.js
@@ -4,45 +4,49 @@
  * @return {number}
  */
 var countComponents = function(n, edges) {
+  
   let par = [];
   for (let i = 0; i < n; i++) {
-    par.push(i);
+    par[i] = i;
   }
   
   let rank = Array(n).fill(1);
   
-  let res = n;
   
+  function findParent(n) {
+    let res = n;
+    while (res !== par[res]) {
+      par[res] = par[par[res]];
+      res = par[res];
+    }
+    return res;  
+  }
+  
+  function union(n1, n2) {
+    let p1 = findParent(n1);
+    let p2 = findParent(n2);
+    
+    if (p1 === p2) {
+      return 0;
+    }
+    
+    if (rank[p2] > rank[p1]) {
+      par[p1] = p2;
+      rank[p2] += rank[p1];
+    } else {
+      par[p2] = p1;
+      rank[p1] += rank[p2];
+    }
+    
+    return 1;
+    
+  }
+  
+  
+  let res = n;
   for (let [n1, n2] of edges) {
-    res -= union(n1, n2, rank, par);
+    res -= union(n1, n2); //0
   }
-  
   return res;
+    
 };
-
-
-function find(n, par) {
-  let res = n;
-  while (res !== par[res]) {
-    par[res] = par[par[res]];
-    res = par[res];
-  }
-  return res;
-}
-
-function union(n1, n2, rank, par) {
-  let p1 = find(n1, par);
-  let p2 = find(n2, par);
-  
-  if (p1 === p2) return 0;
-  
-  if (rank[p2] > rank[p1]) {
-    par[p1] = p2;
-    rank[p2] += rank[p1];
-  } else {
-    par[p2] = p1;
-    rank[p1] += rank[p2];
-  }
-  
-  return 1;
-}
