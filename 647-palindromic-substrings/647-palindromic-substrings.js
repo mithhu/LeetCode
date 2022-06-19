80a63317d2bf94cfd0e04cb5249faad8af70864a
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-19 21:43:16 +0600

    Time: 104 ms (63.61%), Space: 42.9 MB (62.82%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.js
+++ 647-palindromic-substrings/647-palindromic-substrings.js
@@ -4,26 +4,25 @@
  */
 var countSubstrings = function(s) {
   let count = 0;
+  
   for (let i = 0; i < s.length; i++) {
     //odd
     count += countPalindromes(i, i, s);
     //even
     count += countPalindromes(i, i + 1, s);
   }
-  return count; 
+  
+  return count;
 };
-
 
 
 function countPalindromes(l, r, s) {
   let res = 0;
-  
-  while (l >= 0 && r < s.length && s[l] === s[r]) {
+  while (l >= 0 && r >= 0 && l < s.length && r < s.length && s[l] === s[r]) {
     res++;
     l--;
     r++;
   }
+  
   return res;
 }
-
-
