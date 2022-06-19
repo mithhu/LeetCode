71364514c5b5bd17440f16bd0aee93b92c1e43f7
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-19 20:13:01 +0600

    Time: 108 ms (53.85%), Space: 45.7 MB (62.42%) - LeetHub

--- 269-alien-dictionary/269-alien-dictionary.js
+++ 269-alien-dictionary/269-alien-dictionary.js
@@ -9,31 +9,30 @@ var alienOrder = function(words) {
       adj[c] = new Set()
     }
   }
-  
+
   for (let i = 0; i < words.length - 1; i++) {
     let w1 = words[i];
     let w2 = words[i + 1];
     let minLen = Math.min(w1.length, w2.length);
-    
-    if (w1.length > w2.length && w1.substring(0, minLen) === w2.substring(0, minLen)) {
+    if (w1.length > w2.length && 
+      w1.slice(0, minLen) === w2.slice(0, minLen)) {
       return "";
     }
     for (let j = 0; j < minLen; j++) {
       if (w1[j] !== w2[j]) {
         adj[w1[j]].add(w2[j]);
         break;
-      }
+      } 
     }
   }
-  
-  let visit = {};
+
   let res = [];
-  
+  let visit = {};
+
   function dfs(c) {
     if (c in visit) {
       return visit[c];
     }
-    
     visit[c] = true;
     for (let nei of adj[c]) {
       if (dfs(nei)) {
@@ -43,13 +42,14 @@ var alienOrder = function(words) {
     visit[c] = false;
     res.push(c);
   }
-  
+
   for (let c in adj) {
     if (dfs(c)) {
       return "";
     }
   }
-  
+
   return res.reverse().join("");
     
 };
+
