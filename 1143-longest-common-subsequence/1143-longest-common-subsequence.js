9051a71b9eda68cda4f6c46f8bd6d0ced01470b5
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-20 04:27:34 +0600

    Time: 140 ms (55.98%), Space: 53.7 MB (47.80%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.js
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.js
@@ -4,18 +4,18 @@
  * @return {number}
  */
 var longestCommonSubsequence = function(text1, text2) {
-  let dp = Array(text1.length + 1).fill([]).map(() => Array(text2.length + 1).fill(0));
+  let dp = Array(text1.length + 1).fill().map(() => Array(text2.length + 1).fill(0));
   
-  for (let i = 1; i < dp.length; i++) {
-    for (let j = 1; j < dp[0].length; j++) {
-      if (text1[i - 1] === text2[j - 1]) {
-        dp[i][j] = 1 + dp[i - 1][j - 1];
+  for (let t1 = 1; t1 <= text1.length; t1++) {
+    for (let t2 = 1; t2 <= text2.length; t2++) {
+      if (text1[t1 - 1] === text2[t2 - 1]) {
+        dp[t1][t2] = 1 + dp[t1 - 1][t2 - 1];
       } else {
-        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+        dp[t1][t2] = Math.max(dp[t1 - 1][t2], dp[t1][t2 - 1]);
       }
     }
   }
+  
   return dp.at(-1).at(-1);
-
     
 };
