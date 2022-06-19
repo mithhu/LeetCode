dac2d0a36ef1f856036edeb18530815b7437c151
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-19 19:05:20 +0600

    Time: 116 ms (37.53%), Space: 46.9 MB (40.09%) - LeetHub

--- 261-graph-valid-tree/261-graph-valid-tree.js
+++ 261-graph-valid-tree/261-graph-valid-tree.js
@@ -1,49 +1,40 @@
-/**
- * @param {number} n
- * @param {number[][]} edges
- * @return {boolean}
- */
 var validTree = function(n, edges) {
-  if (n === null) return true;
+  if (n === null) return 0;
+  
   const graph = buildGraph(n, edges);
-  
-  let visit = new Set();
-  
-  function dfs(i, prev) {
-    if (visit.has(i)) {
+  const set = new Set();
+
+  function dfs(n, prev) {
+    if (set.has(n)) {
       return false;
     }
-    
-    visit.add(i);
-    for (let nei of graph[i]) {
-      if (prev === nei) {
+    set.add(n);
+    for (let nei of graph[n]) {
+      if (nei === prev) {
         continue;
       }
-      if (!dfs(nei, i)) {
+      if (!(dfs(nei, n))) {
         return false;
       }
     }
     return true;
-  }
-    
-    return dfs(0, -1) && visit.size === n;
-    
-    
-};
 
+  }
+
+  return dfs(0, -1) && set.size === n
+};
 
 function buildGraph(n, edges) {
   const graph = {};
   for (let i = 0; i < n; i++) {
     graph[i] = [];
   }
-  
-  for (let edge of edges) {
-    const [n1, n2] = edge;
+  for (let [n1, n2] of edges) {
     graph[n1].push(n2);
-    graph[n2].push(n1);
-  }
-  
+    graph[n2].push(n1);    
+  }  
+
+
   return graph;
 }
 
