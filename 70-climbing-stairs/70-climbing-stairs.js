9627fd5591b05a842fb36e46560c7380ff4061ff
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-19 20:35:09 +0600

    Time: 90 ms (32.93%), Space: 41.9 MB (56.44%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.js
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -3,15 +3,14 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-  let one = 1;
-  let two = 1;
+  let one = 1; //0
+  let two = 1; //1
   
   for (let i = 2; i < n + 1; i++) {
     let temp = two;
     two = one + two;
     one = temp;
   }
-  
   return two;
     
 };
