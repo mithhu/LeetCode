64c326e1bade8108176d0f44c65b9f53dc7589e7
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-20 17:49:29 +0600

    Time: 855 ms (67.30%), Space: 96.3 MB (48.32%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.js
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.js
@@ -5,29 +5,30 @@ class MedianFinder {
     this.large = new MinPriorityQueue();
   }
   
-  
   addNum(num) {
     if (this.small.isEmpty() || this.small.front().element >= num) {
       this.small.enqueue(num);
     } else {
       this.large.enqueue(num);
-    }  
+    }
     
     if (this.small.size() > this.large.size() + 1) {
-      let val = this.small.dequeue().element;
-      this.large.enqueue(val);
+      let elem = this.small.dequeue().element;
+      this.large.enqueue(elem);
     } else if (this.small.size() < this.large.size()) {
-      let val = this.large.dequeue().element;
-      this.small.enqueue(val);
+      let elem = this.large.dequeue().element;
+      this.small.enqueue(elem);
     }
   }
   
   findMedian() {
-    //2 bucket equal
-    if (this.small.size() === this.large.size()) {
-      return (this.small.front().element + this.large.front().element) / 2;
+    if ((this.small.size() + this.large.size()) % 2) {
+      return this.small.front().element;
     }
-    return this.small.front().element;
-  }
     
+    return (this.small.front().element + this.large.front().element) / 2;
+  }
 };
+
+
+
