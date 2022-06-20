07280d88fff1190b3c26caf87424414364dbae62
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-21 03:53:24 +0600

    Time: 116 ms (40.04%), Space: 49.7 MB (46.40%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.js
+++ 11-container-with-most-water/11-container-with-most-water.js
@@ -4,12 +4,13 @@
  */
 var maxArea = function(height) {
   let maxArea = 0;
+  
   let l = 0;
   let r = height.length - 1;
   
   while (l < r) {
     let h = Math.min(height[l], height[r]);
-    let w = r - l;
+    let w = r - l; 
     maxArea = Math.max(maxArea, h * w);
     
     if (height[l] <= height[r]) {
