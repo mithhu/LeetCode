7c52f0ce50e646b86b6b7530adbdc51958a8e5cd
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-20 19:56:45 +0600

    Time: 1619 ms (44.65%), Space: 49 MB (55.96%) - LeetHub

--- 212-word-search-ii/212-word-search-ii.js
+++ 212-word-search-ii/212-word-search-ii.js
@@ -3,24 +3,23 @@
  * @param {string[]} words
  * @return {string[]}
  */
+
 class TrieNode {
   constructor() {
-    this.children = {};
+    this.childrens = {};
     this.word = false;
   }
-  
   addWord(word) {
     let curr = this;
     for (let ch of word) {
-      if (!(ch in curr.children)) {
-        curr.children[ch] = new TrieNode();
+      if (!(ch in curr.childrens)) {
+        curr.childrens[ch] = new TrieNode();
       }
-      curr = curr.children[ch];
+      curr = curr.childrens[ch];
     }
     curr.word = true;
   }
 }
-
 var findWords = function(board, words) {
   let root = new TrieNode();
   for (let word of words) {
@@ -28,38 +27,35 @@ var findWords = function(board, words) {
   }
   
   let res = [];
-  
   let ROWS = board.length;
   let COLS = board[0].length;
   
   function dfs(r, c, node, word) {
-    if (r < 0 || c < 0 || r >= ROWS || c >= COLS || !board[r][c] || !(board[r][c] in node.children)) {
+    if (r < 0 || c < 0 || r >= ROWS || c >= COLS || !board[r][c] || !(board[r][c] in node.childrens)) {
       return;
     }
-    node = node.children[board[r][c]];
+    node = node.childrens[board[r][c]];
     word += board[r][c];
-    
     if (node.word) {
       res.push(word);
       node.word = false;
     }
-  
-  
     let temp = board[r][c];
-    board[r][c] = null;   
+    board[r][c] = null;
     dfs(r + 1, c, node, word);
     dfs(r - 1, c, node, word);
     dfs(r, c + 1, node, word);
     dfs(r, c - 1, node, word);
-    board[r][c] = temp;   
+    board[r][c] = temp;
   }
   
-  for (let i = 0; i < ROWS; i++) {
-    for (let j = 0; j < COLS; j++) {
-      dfs(i, j, root, "");
+  
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[0].length; j++) {
+      dfs(i, j, root, "")
     }
   }
   
   return res;
-  
+    
 };
