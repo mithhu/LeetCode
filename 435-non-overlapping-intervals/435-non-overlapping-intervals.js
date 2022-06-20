9a87340ffcef272252d6922bf2d9ac3de6da00c7
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-20 12:52:48 +0600

    Time: 266 ms (91.86%), Space: 78.6 MB (14.43%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.js
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.js
@@ -5,17 +5,19 @@
 var eraseOverlapIntervals = function(intervals) {
   intervals.sort((a, b) => a[0] - b[0]);
   
-  let res = 0;
   let prevEnd = intervals[0][1];
+  let count = 0;
   
-  for (let [start, end] of intervals.slice(1)) {
+  for (let i = 1; i < intervals.length; i++) {
+    let [start, end] = intervals[i];
     if (start >= prevEnd) {
       prevEnd = end;
     } else {
-      res++;
-      prevEnd = Math.min(prevEnd, end)
+      count++;
+      prevEnd = Math.min(prevEnd, end);
     }
   }
-  return res;
+  
+  return count;
     
 };
