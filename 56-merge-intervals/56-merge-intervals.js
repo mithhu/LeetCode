8b74e0d93df0ee4533c486c57c6134de446ff635
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-20 12:37:39 +0600

    Time: 161 ms (45.26%), Space: 48.6 MB (78.83%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.js
+++ 56-merge-intervals/56-merge-intervals.js
@@ -4,20 +4,19 @@
  */
 var merge = function(intervals) {
   intervals.sort((a, b) => a[0] - b[0]);
+  let res = [];
+  res.push(intervals[0]);
   
-  let res = [intervals[0]];
   
   for (let i = 1; i < intervals.length; i++) {
-    let lastEnd = res.at(-1);
-    let [start, end] = intervals[i];
+    let [s, e] = intervals[i];
+    let lastInterval = res.at(-1);
     
-    if (start <= lastEnd[1]) {
-      lastEnd[1] = Math.max(lastEnd[1], end);
+    if (s <= lastInterval[1]) {
+      lastInterval[1] = Math.max(lastInterval[1], e);
     } else {
-      res.push(intervals[i])
+      res.push(intervals[i]);
     }
-
   }
-  
-  return res;
+  return res; 
 };
