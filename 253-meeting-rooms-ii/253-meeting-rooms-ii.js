b801f6c93acae4166e46fa05dcbe4f3513b2b173
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-20 13:13:41 +0600

    Time: 115 ms (45.26%), Space: 44.5 MB (68.61%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.js
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.js
@@ -3,23 +3,24 @@
  * @return {number}
  */
 var minMeetingRooms = function(intervals) {
-  let start = intervals.map((i) => i[0]).sort((a, b) => a - b);
-  let end = intervals.map((i) => i[1]).sort((a, b) => a - b);
+  let start = intervals.map((el) => el[0]).sort((a, b) => a - b);
+  let end = intervals.map((el) => el[1]).sort((a, b) => a - b);
   
-  let count = 0;
   let res = 0;
-  let s = 0;
-  let e = 0;
+  let count = 0;
+  let sIdx = 0;
+  let eIdx = 0;
   
-  while (s < start.length) {
-    if (start[s] < end[e]) {
+  while (sIdx < start.length) {
+    if (start[sIdx] < end[eIdx]) {
       count++;
-      s++;
+      sIdx++;
       res = Math.max(res, count);
     } else {
       count--;
-      e++;
+      eIdx++;
     }
   }
+  
   return res;
 };
