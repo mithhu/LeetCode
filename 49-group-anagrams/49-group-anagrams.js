bc65759d3a748703a21b916c1b65955c64b622e1
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-21 00:46:20 +0600

    Time: 401 ms (6.26%), Space: 54.5 MB (28.33%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.js
+++ 49-group-anagrams/49-group-anagrams.js
@@ -6,18 +6,19 @@
 function groupAnagrams(strs) {
   let res = {};
   
-  for (let s of strs) {
+  for (let s of strs)  {
     let count = Array(26).fill(0);
-    
-    for (let c of s) {
-      let idx = c.charCodeAt() - "a".charCodeAt();
-      count[idx] += 1; 
+    for (let ch of s) {
+      let idx = ch.charCodeAt() - "a".charCodeAt();
+      count[idx]++;
     }
+    
     if (!(count in res)) {
       res[count] = [];
     }
-    res[count].push(s); 
+    res[count].push(s);
   }
   
   return Object.values(res);
+  
 }
