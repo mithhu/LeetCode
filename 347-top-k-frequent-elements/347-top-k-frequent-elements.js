193debed3ae92b965a3cec7f1577fa7c35352bf8
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-21 01:05:26 +0600

    Time: 166 ms (13.23%), Space: 48.8 MB (19.83%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.js
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.js
@@ -4,22 +4,26 @@
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) {
-  let count = _.countBy(nums);
-  let freq = Array(nums.length + 1).fill().map(() => []);
+  let count = {};
+  for (let num of nums) {
+    if (!(num in count)) {
+      count[num] = 0; 
+    }
+    count[num]++;
+  }
   
+  let freq = Array(nums.length + 1).fill().map(() => []);
   for (let key in count) {
     freq[count[key]].push(key);
   }
-  
-  
   let res = [];
-  
-  for (i = freq.length - 1; i >= 0; i--) {
-    for (let n of freq[i]) {
-      res.push(n);
+  for (let i = freq.length - 1; i >= 0; i--) {
+    for (let num of freq[i]) {
+      res.push(num);
       if (res.length === k) {
         return res;
       }
     }
-  } 
+  }
+    
 };
