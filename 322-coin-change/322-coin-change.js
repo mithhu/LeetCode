9d35fb3d8ab0cd04aa4e712b294d5fb162fc456a
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-22 00:46:14 +0600

    Time: 189 ms (45.86%), Space: 47.5 MB (56.37%) - LeetHub

--- 322-coin-change/322-coin-change.js
+++ 322-coin-change/322-coin-change.js
@@ -4,17 +4,24 @@
  * @return {number}
  */
 var coinChange = function(coins, amount) {
-  let dp = Array(amount + 1).fill(Infinity);
-  dp[0] = 0;
+  // 1 2 5 -> 11
+  // // 5 5 1
+  // 5 222
   
-  for (let a = 0; a <= amount; a++) {
-    for (let c of coins) {
-      if (a - c >= 0) {
-        dp[a] = Math.min(dp[a], 1 + dp[a - c])
+  //for -- 0 - amount
+   // each coin //4
+  
+  let res = Array(amount + 1).fill(Infinity);
+  res[0] = 0;
+  
+
+  for (let i = 0; i < amount + 1; i++) {
+    for (let coin of coins) {
+      if (i - coin >= 0) {
+        res[i] = Math.min(1 + res[i - coin], res[i]);
       }
     }
   }
   
-  return dp[amount] === Infinity ? -1 : dp[amount];
-    
+  return res[amount] === Infinity ? -1 : res[amount];
 };
