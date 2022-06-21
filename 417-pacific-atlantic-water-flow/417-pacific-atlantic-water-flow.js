6076710d2e3075923bd0f79edf0d1b5b7c8ef812
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-22 00:05:16 +0600

    Time: 251 ms (36.29%), Space: 51 MB (72.78%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.js
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.js
@@ -3,47 +3,48 @@
  * @return {number[][]}
  */
 var pacificAtlantic = function(heights) {
-  let atl = new Set();
-  let pac = new Set();
+  //pac
+  //atl
   let R = heights.length;
   let C = heights[0].length;
+  let atl = new Set();
+  let pac = new Set();
   
   //first and last row
   for (let c = 0; c < C; c++) {
-    //pac
-    dfs(0, c, pac, heights[0][c], heights);
-    //atl
-    dfs(R - 1, c, atl, heights[R - 1][c], heights);
+    dfs(0, c, heights[0][c], pac);
+    dfs(R - 1, c, heights[R - 1][c], atl);
   }
   
-  //first and alst col
+  
+  //first and last col
   for (let r = 0; r < R; r++) {
-    dfs(r, 0, pac, heights[r][0], heights);
-    dfs(r, C - 1, atl, heights[r][C - 1], heights); 
+    dfs(r, 0, heights[r][0], pac);
+    dfs(r, C - 1, heights[r][C - 1], atl);
+  }
+  
+  
+  function dfs(r, c, prevH, set) {
+    if (r < 0 || c < 0 || r >= R || c >= C || prevH > heights[r][c] || set.has(`${r}*${c}`)) {
+      return false;
+    }
+    set.add(`${r}*${c}`);
+    dfs(r + 1, c, heights[r][c], set);
+    dfs(r - 1, c, heights[r][c], set);
+    dfs(r, c + 1, heights[r][c], set);
+    dfs(r, c - 1, heights[r][c], set); 
   }
   
   let res = [];
-  
   for (let r = 0; r < R; r++) {
     for (let c = 0; c < C; c++) {
       if (atl.has(`${r}*${c}`) && pac.has(`${r}*${c}`)) {
-        res.push([r, c]);
+        res.push([r, c])
       }
     }
   }
   
   return res;
-    
+  
+  
 };
-
-
-function dfs(r, c, set, prevH, heights) {
-  if (r < 0 || r >= heights.length || c < 0 || c >= heights[0].length || set.has(`${r}*${c}`) || heights[r][c] < prevH) {
-    return;
-  }
-  set.add(`${r}*${c}`);
-  dfs(r + 1, c, set, heights[r][c], heights);
-  dfs(r - 1, c, set, heights[r][c], heights);
-  dfs(r, c + 1, set, heights[r][c], heights);
-  dfs(r, c - 1, set, heights[r][c], heights);
-}
