a81466df25756593eac0928e8c0ccfb693046364
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-22 03:59:22 +0600

    Time: 202 ms (84.68%), Space: 69.2 MB (14.85%) - LeetHub

--- 127-word-ladder/127-word-ladder.js
+++ 127-word-ladder/127-word-ladder.js
@@ -9,52 +9,45 @@ var ladderLength = function(beginWord, endWord, wordList) {
     return 0;
   }
   
-  wordList.push(beginWord);
-  
   let nei = {};
+  wordList.push(beginWord);
   
   for (let word of wordList) {
     for (let i = 0; i < word.length; i++) {
-      let pattern = word.substring(0, i) + "*" + word.substring(i + 1);
+      let pattern = word.slice(0, i) + "*" + word.slice(i + 1);
       if (!(pattern in nei)) {
-        nei[pattern] = [];
+        nei[pattern] = []
       }
       nei[pattern].push(word);
     }
   }
   
-
-  let visited = new Set();
-  visited.add(beginWord);
   let q = [];
   q.push(beginWord);
   let res = 1;
+  let visited = new Set();
   
-  
-  while (q.length > 0) {
+  while (q.length) {
     let len = q.length;
     for (let i = 0; i < len; i++) {
-      const word = q.shift();
-      if (word === endWord) return res;
-      
-      for (let j = 0; j < word.length; j++) {
-        let pattern = word.substring(0, j) + "*" + word.substring(j + 1);
-        for (let neiWord of nei[pattern]) {
-          if (!(visited.has(neiWord))) {
-            q.push(neiWord);
-            visited.add(neiWord);
+      let word = q.shift();
+      if (word === endWord) {
+        return res;
+      }
+      for (let i = 0; i < word.length; i++) {
+        let pattern = word.slice(0, i) + "*" + word.slice(i + 1);
+        for (let n of nei[pattern]) {
+          if (!visited.has(n)) {
+            q.push(n);
+            visited.add(n);
           }
         }
       }
       
     }
-    
-    res += 1;
+    res++;
   }
   
   return 0;
-  
-
-
-  
+    
 };
