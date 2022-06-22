e79d33996e5f5e7fa0ad04afef15242b907a6488
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-22 12:55:05 +0600

    Time: 75 ms (59.53%), Space: 41.7 MB (91.68%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.js
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.js
@@ -3,33 +3,34 @@
  * @return {string[]}
  */
 var letterCombinations = function(digits) {
-  let digitToChar = { "2": "abc",
-                        "3": "def",
-                        "4": "ghi",
-                        "5": "jkl",
-                        "6": "mno",
-                        "7": "qprs",
-                        "8": "tuv",
-                        "9": "wxyz" }
+  let digitsMap = {
+    "2": "abc",
+    "3": "def",
+    "4": "ghi",
+    "5": "jkl",
+    "6": "mno",
+    "7": "pqrs",
+    "8": "tuv",
+    "9": "wxyz",
+  }
   let res = [];
   
-  function dfs(i, curStr) {
-    if (curStr.length === digits.length) {
-      res.push(curStr);
+  function dfs(idx, currStr) {
+    if (currStr.length === digits.length) {
+      res.push(currStr);
       return;
     }
     
-    
-    for (let c of digitToChar[digits[i]]) {
-      dfs(i + 1, curStr + c)
+    for (let char of digitsMap[digits[idx]]) {
+      dfs(idx + 1, currStr + char)
     }
-    
   }
   
   if (digits.length) {
-    dfs(0, "") 
+    dfs(0, "");
   }
   
-  return res; //[]
+  return res;
+  
     
 };
