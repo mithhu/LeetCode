7c842cb9c3495c7594803614bd6fa5efd84bc7cc
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-23 03:01:44 +0600

    Time: 128 ms (43.41%), Space: 45.4 MB (52.85%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.js
+++ 695-max-area-of-island/695-max-area-of-island.js
@@ -2,33 +2,30 @@
  * @param {number[][]} grid
  * @return {number}
  */
-
 var maxAreaOfIsland = function(grid) {
-  if (grid.length === 0) return 0;
-  let ROWS = grid.length;
-  let COLS = grid[0].length;
-  let count = 0;
-  
-  function dfs(r, c) {
-    if (r < 0 || r >= ROWS || c < 0 || c >= COLS || grid[r][c] === 0) return 0;
-    grid[r][c] = 0;
-    
-    return 1 +  dfs(r + 1, c) +
-    dfs(r - 1, c) +
-    dfs(r, c + 1) +
-    dfs(r, c - 1);
-  }
-  
+  let R = grid.length;
+  let C = grid[0].length;
   let area = 0;
   
-  for (let r = 0; r < ROWS; r++) {
-    for (let c = 0; c < COLS; c++) {
+  function dfs(r, c) {
+    if (r < 0 || r >= R || c < 0 || c >= C || grid[r][c] === 0) {
+      return 0;
+    }
+    grid[r][c] = 0;
+    return 1 + dfs(r + 1, c) + dfs(r - 1, c) + dfs(r, c + 1) + dfs(r, c - 1); 
+  }
+  
+  
+  
+  for (let r = 0; r < R; r++) {
+    for (let c = 0; c < C; c++) {
       if (grid[r][c] === 1) {
-        area = Math.max(area, dfs(r, c))
+        area = Math.max(area, dfs(r, c));
       }
     }
   }
   
   return area;
+  
     
 };
