4743893f68deee311cf583689566ae98c9116b15
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-22 20:53:04 +0600

    Time: 491 ms (32.76%), Space: 47.9 MB (74.53%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.js
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.js
@@ -3,27 +3,28 @@
  * @return {boolean}
  */
 var canPartition = function(nums) {
-  const sum = nums.reduce((sum, curr) => curr + sum, 0);
-  
-  if (sum % 2) return false;
-  
+  let sum = nums.reduce((prev, curr) => prev + curr, 0);
+  if (sum % 2) {
+    return false;
+  }
   let target = sum / 2;
-  console.log(target)
-  
   let dp = new Set();
   dp.add(0);
   
-  for (let num of nums) { 
+  // 0, 1, 0 + 5, 1 + 5
+  
+  for (let num of nums) {
     let tempSet = new Set([...dp]);
-    for (let val of dp) { 
+    for (let val of dp) {
       if (val + num === target) {
         return true;
       } else if (val + num < target) {
-        tempSet.add(val + num);        
+        tempSet.add(val + num);
       }
-    }    
-    dp = tempSet; 
+    }
+    dp = tempSet;
   }
   
-  return dp.has(target); 
+  return false;
+    
 };
