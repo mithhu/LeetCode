af2f559099129c8486625deb71ecc6ed2ea0f0ea
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-23 00:55:55 +0600

    Time: 85 ms (45.64%), Space: 42 MB (64.06%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.js
+++ 213-house-robber-ii/213-house-robber-ii.js
@@ -3,19 +3,25 @@
  * @return {number}
  */
 var rob = function(nums) {
+  let rob1 = robValue(nums.slice(0, nums.length - 1));
+  let rob2 = robValue(nums.slice(1));
   
-  return Math.max(nums[0], robHelper(nums.slice(0, nums.length - 1)), robHelper(nums.slice(1)));
+  return Math.max(nums[0], rob1, rob2);
     
 };
 
-function robHelper(arr) {
-  let rob1 = 0;
-  let rob2 = 0;
+function robValue(nums) {
+  let one = 0;
+  let two = 0;
+
   
-  for (let money of arr) {
-    let temp = Math.max(money + rob1, rob2);
-    rob1 = rob2;
-    rob2 = temp;
+  for (let num of nums) {
+    let temp = Math.max(num + one, two);
+    one = two;
+    two = temp;
   }
-  return rob2;
+  return two;
 }
+
+
+// 1 2 3 4 5 6 9
