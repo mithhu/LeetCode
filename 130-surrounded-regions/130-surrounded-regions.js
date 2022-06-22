39d926b067d3c61ff0103778be554e8a99a834a2
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-23 03:36:58 +0600

    Time: 116 ms (63.49%), Space: 46.5 MB (72.77%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.js
+++ 130-surrounded-regions/130-surrounded-regions.js
@@ -3,36 +3,36 @@
  * @return {void} Do not return anything, modify board in-place instead.
  */
 var solve = function(board) {
-  let ROWS = board.length;
-  let COLS = board[0].length;
+  let R = board.length;
+  let C = board[0].length;
   
-  function dfs(r, c) {    
-    if (r < 0 || c < 0 || r >= ROWS || c >= COLS || board[r][c] !== "O") {
+  function dfs(r, c) {
+    if (r < 0 || c < 0 || r >= R || c >= C || board[r][c] !== "O") {
       return;
     }
-    board[r][c] = "T";
+    board[r][c] = "I";
     dfs(r + 1, c);
     dfs(r - 1, c);
     dfs(r, c + 1);
     dfs(r, c - 1);
   }
   
-  for (let r = 0; r < ROWS; r++) {
-    for (let c = 0; c < COLS; c++) {
-      if (board[r][c] === "O" && (r === 0 || r === ROWS - 1 || c === 0 || c === COLS - 1)) {
+  for (let r = 0; r < R; r++) {
+    for (let c = 0; c < C; c++) {
+      if (board[r][c] === "O" && (r === 0 || c === 0 || r === R - 1 || c === C - 1)) {
         dfs(r, c);
       }
     }
   }
   
-  for (let r = 0; r < ROWS; r++) {
-    for (let c = 0; c < COLS; c++) {
+ for (let r = 0; r < R; r++) {
+    for (let c = 0; c < C; c++) {
       if (board[r][c] === "O") {
-        board[r][c] = "X";
-      } else if (board[r][c] === "T") {
+        board[r][c] = "X"
+      } else if (board[r][c] === "I") {
         board[r][c] = "O"
       }
     }
-  }
+ }
     
 };
