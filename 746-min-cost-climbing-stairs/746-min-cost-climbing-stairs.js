b4aabd23b1ba86e0660daa21a4c266dfae2b9551
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-23 05:00:14 +0600

    Time: 142 ms (5.06%), Space: 42.4 MB (89.10%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
@@ -7,9 +7,9 @@ var minCostClimbingStairs = function(cost) {
   let first = cost[0];
   let second = cost[1];
   
-  if (n < 2) {
-    return Math.min(first, second);
-  }
+  // if (n < 2) {
+  //   return Math.min(first, second);
+  // }
  
   for (let i = 2; i < n; i++) {
     let curr = cost[i] + Math.min(first, second);
