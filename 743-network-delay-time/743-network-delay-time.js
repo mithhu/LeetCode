728659f3e940b334e9bf5a53eb1168d44467be0c
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-23 13:43:31 +0600

    Time: 115 ms (87.73%), Space: 51.1 MB (47.96%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.js
+++ 743-network-delay-time/743-network-delay-time.js
@@ -11,28 +11,29 @@ var networkDelayTime = function(times, n, k) {
   }
   
   for (let [u, v, w] of times) {
-    edges[u].push([v, w]);
+    edges[u].push([w, v]);
   }
   
-  let minHeap = new MinPriorityQueue({priority: (elem) => elem[0]});
-  minHeap.enqueue([0, k]);
-  let visited = new Set();
   let res = 0;
+  let visit = new Set();
+  let minH = new MinPriorityQueue({priority: (elem) => elem[0]}); // [cost, n]
+  minH.enqueue([0, k]);
   
-  while (minHeap.size() && visited.size < n) {
-    let [w1, n1] = minHeap.dequeue().element;
-    if (visited.has(n1)) {
+  while (minH.size() && visit.size < n) {
+    let [w1, n1] = minH.dequeue().element;
+    if (visit.has(n1)) {
       continue;
     }
-    visited.add(n1);
     res = w1;
-    
-    for (let [n2, w2] of edges[n1]) {
-      if (!(visited.has(n2))) {
-        minHeap.enqueue([w1 + w2, n2])
+    visit.add(n1);
+    for (let [w2, n2] of edges[n1]) {
+      if (!(visit.has(n2))) {
+        minH.enqueue([w2 + w1, n2]);
       }
     }
   }
-  return visited.size === n ? res : -1;
+  
+  console.log(visit.size, res)
+  return visit.size === n ? res : -1;
+    
 };
-
