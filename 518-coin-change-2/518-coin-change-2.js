9f6871155be6786df8f4c5aa6b9466996c81704a
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-23 20:36:23 +0600

    Time: 138 ms (48.82%), Space: 42.5 MB (67.92%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.js
+++ 518-coin-change-2/518-coin-change-2.js
@@ -3,21 +3,36 @@
  * @param {number[]} coins
  * @return {number}
  */
+// var change = function(amount, coins) {
+//   let dp = Array(coins.length + 1).fill([]).map(() => Array(amount + 1).fill(0));
+//   for (let r = 0; r < coins.length + 1; r++) {
+//     dp[r][0] = 1;
+//   }
+  
+//   for (let c = 1; c < coins.length + 1; c++) {
+//     for (let a = 1; a < amount + 1; a++) {
+//       if (coins[c - 1] <= a) {
+//         dp[c][a] = dp[c - 1][a] + dp[c][a - coins[c - 1]];
+//       } else {
+//         dp[c][a] = dp[c - 1][a];
+//       }
+//     }
+//   }  
+//   return dp.at(-1).at(-1);
+    
+// };
+
 var change = function(amount, coins) {
-  let dp = Array(coins.length + 1).fill([]).map(() => Array(amount + 1).fill(0));
-  for (let r = 0; r < coins.length + 1; r++) {
-    dp[r][0] = 1;
-  }
+  let dp = Array(amount + 1).fill(0);
+  dp[0] = 1;
   
   for (let c = 1; c < coins.length + 1; c++) {
-    for (let a = 1; a < amount + 1; a++) {
+    for (let a = 1; a <  amount + 1; a++) {
       if (coins[c - 1] <= a) {
-        dp[c][a] = dp[c - 1][a] + dp[c][a - coins[c - 1]];
-      } else {
-        dp[c][a] = dp[c - 1][a];
-      }
+        dp[a] = dp[a] + dp[a - coins[c - 1]];
+      } 
     }
   }  
-  return dp.at(-1).at(-1);
+  return dp.at(-1);
     
 };
