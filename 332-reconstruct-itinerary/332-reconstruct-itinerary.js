58e80bc4c35dfa61573d2cedb2077238048d20a9
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-23 12:17:53 +0600

    Time: 123 ms (57.39%), Space: 49.4 MB (36.45%) - LeetHub

--- 332-reconstruct-itinerary/332-reconstruct-itinerary.js
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.js
@@ -8,7 +8,7 @@ var findItinerary = function(tickets) {
   let res = ["JFK"];
   
   for (let [u, v] of tickets) {
-    if(!(u in adj)) {
+    if (!(u in adj)) {
       adj[u] = [];
     }
     adj[u].push(v);
@@ -18,23 +18,23 @@ var findItinerary = function(tickets) {
     if (res.length === tickets.length + 1) {
       return true;
     }
-    
     if (!(cur in adj)) {
       return false;
     }
     
     let temp = [...adj[cur]];
+    
     for (let v of temp) {
-      adj[cur].shift(); // if it does not work, then I move it to last. coz in next iteration I need new value in shift. If I put back to first, then the value will be the same. Now how it wokrs on push? - works coz we are looping, so all that value will work will be rmoved from shift method. And those who don't will start at the end of the array. say = [1, 2, 3, 4] -> 1, 3 does not work when 1 does not work, the value becomes - [2, 3, 4, 1], 2 works so remove it = [3, 4, 1]. 3 does not so push = [4, 1, 2]. 4 works so remove = [1, 2]. see still sorted
-      
+      adj[cur].shift();
       res.push(v);
-      if (dfs(v)) {
-        return res;
-      }
-      res.pop();
-      adj[cur].push(v);
       
+      if (dfs(v)) {
+        return true;
+      }
+      adj[cur].push(v);
+      res.pop();
     }
+    
     return false;
   }
   
