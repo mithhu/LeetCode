5fa28560579aa10d28a561caafe759e0ec0978f1
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-24 03:55:43 +0600

    Time: 77 ms (84.79%), Space: 44.3 MB (47.11%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.js
+++ 45-jump-game-ii/45-jump-game-ii.js
@@ -5,18 +5,17 @@
 var jump = function(nums) {
   let l = 0;
   let r = 0;
-  let res = 0;
+  let farthest = 0;
+  let level = 0;
   
   while (r < nums.length - 1) {
-    let farthest = 0;
     for (let i = l; i <= r; i++) {
       farthest = Math.max(farthest, i + nums[i]);
     }
     l = r + 1;
     r = farthest;
-    res++;
+    level++;
   }
-  
-  return res;
+  return level;
     
 };
