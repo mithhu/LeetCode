4502ca980a249e8b9bbf33b00c5d8f8a3cabcc50
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-23 21:15:38 +0600

    Time: 816 ms (53.41%), Space: 54 MB (13.00%) - LeetHub

--- 494-target-sum/494-target-sum.js
+++ 494-target-sum/494-target-sum.js
@@ -4,21 +4,22 @@
  * @return {number}
  */
 var findTargetSumWays = function(nums, target) {
-  let dp = {};
+  let cache = {};
   
   function backtrack(i, total) {
     if (i === nums.length) {
-      return total === target ? 1 : 0;
+       return total === target ? 1 : 0;
     }
     
-    if (`${i}*${total}` in dp) {
-      return dp[`${i}*${total}`];
+    if (`${i}_${total}`in cache) {
+      return cache[`${i}_${total}`];
     }
     
-    dp[`${i}*${total}`] = backtrack(i + 1, total + nums[i]) + backtrack(i + 1, total - nums[i]);
-    return dp[`${i}*${total}`];
+    cache[`${i}_${total}`] = backtrack(i + 1, total + nums[i]) + backtrack(i + 1, total - nums[i]);
+    return cache[`${i}_${total}`];
   }
   
   return backtrack(0, 0);
+
     
 };
