d6b7aa9f2a83744066bf03be19f366490a2c7a3c
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-24 12:55:00 +0600

    Time: 961 ms (25.53%), Space: 125.5 MB (23.40%) - LeetHub

--- 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.js
+++ 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.js
@@ -6,39 +6,35 @@
 var minInterval = function(intervals, queries) {
   intervals.sort((a, b) => {
     if (a[0] !== b[0]) {
-      return a[0] - b[0]
+      return a[0] - b[0];
     }
     return a[1] - b[1];
-  });
-  let res = {};
+  })
+  
   let i = 0;
-  let minHeap = new MinPriorityQueue({compare: (a, b) => {
-    if (a[0] !== b[0]) {
-      return a[0] - b[0]
-    }
-    return a[1] - b[1];
-  }});
+  let minHeap = new MinPriorityQueue({priority: (el) => el[0]});
+  let res = {};
   
-  
-  
-  for (let q  of [...queries].sort((a, b) => a - b)) {
+  for (let q of [...queries].sort((a, b) => a -b)) {
     while (i < intervals.length && intervals[i][0] <= q) {
       let [l, r] = intervals[i];
       minHeap.enqueue([r - l + 1, r]);
       i++;
     }
     
-    while (minHeap.size() && minHeap.front()[1] < q) {
+    while (minHeap.size() && minHeap.front().element[1] < q) {
       minHeap.dequeue();
     }
-    res[q] = minHeap.size() ? minHeap.front()[0] : -1;
+    
+    res[q] = minHeap.size() ? minHeap.front().element[0] : - 1;
   }
   
-  let result = [];
   
+  let result = [];
   for (let q of queries) {
     result.push(res[q]);
   }
   
   return result;
+    
 };
