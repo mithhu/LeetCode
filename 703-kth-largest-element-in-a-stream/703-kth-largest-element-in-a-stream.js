b1ed6ce1f8a4f5ed6b6faf0dbdc8ad975621f1c9
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-24 21:40:35 +0600

    Time: 301 ms (40.33%), Space: 52.3 MB (38.01%) - LeetHub

--- 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.js
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.js
@@ -2,33 +2,23 @@
  * @param {number} k
  * @param {number[]} nums
  */
-class KthLargest{
-  constructor(k, nums) {
-    this.k = k;
-    this.minHeap = new MinPriorityQueue();
-    
-    for (let num of nums) {
-      this.add(num);
-    }
-  }
-  
-  
-  add(val) {
-    this.minHeap.enqueue(val);
-    if (this.minHeap.size() > this.k) {
-      this.minHeap.dequeue();
-    }
-    
-    return this.minHeap.front().element;
-    
-  }
+var KthLargest = function(k, nums) {
+  this.k = k;
+  this.minHeap = new MinPriorityQueue();
+  nums.forEach((num) => this.add(num))
 };
 
 /** 
  * @param {number} val
  * @return {number}
  */
-
+KthLargest.prototype.add = function(val) {
+  this.minHeap.enqueue(val);
+  if (this.minHeap.size() > this.k) {
+    this.minHeap.dequeue();
+  }
+  return this.minHeap.front().element;
+};
 
 /** 
  * Your KthLargest object will be instantiated and called as such:
