56130f3f0183914d0b9c1325b0faf7aad1c6468f
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-24 16:41:29 +0600

    Time: 127 ms (14.86%), Space: 45 MB (34.18%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.js
+++ 90-subsets-ii/90-subsets-ii.js
@@ -6,21 +6,19 @@
 var subsetsWithDup = function(nums) {
   let res = [];
   let subset = [];
-  nums.sort((a, b) => a - b);
-  function dfs(i) {
-    if (i >= nums.length) {
-      res.push([...subset]);
-      return;
+  nums.sort((a, b) => a -b);
+  
+  function dfs(idx) {
+    res.push([...subset]);
+    for (let i = idx; i < nums.length; i++) {
+      if (i > idx && nums[i] === nums[i - 1]) {
+        continue;
+      }
+      subset.push(nums[i]);
+      dfs(i + 1);
+      subset.pop();
     }
-
-    subset.push(nums[i]);
-    dfs(i + 1);
-    subset.pop();
-    while (i + 1 < nums.length && nums[i] === nums[i + 1]) {
-      i++;
-    }
-    dfs(i + 1);
-  } 
+  }
   
   dfs(0);
   return res;
