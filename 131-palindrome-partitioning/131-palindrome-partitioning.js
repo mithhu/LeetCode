1ae41016529aee5b82b966d67fdd757654de231e
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-24 19:49:45 +0600

    Time: 454 ms (28.90%), Space: 79.8 MB (78.54%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.js
+++ 131-palindrome-partitioning/131-palindrome-partitioning.js
@@ -4,35 +4,34 @@
  */
 var partition = function(s) {
   let res = [];
-  let part = [];
+  let curr = [];
   
-  function dfs(i) {
+  function backtrack(i) {
     if (i === s.length) {
-      res.push([...part]);
-      return;
+      res.push([...curr]);
     }
     
     for (let j = i; j < s.length; j++) {
       if (isPalindrome(s, i, j)) {
-        part.push(s.slice(i, j + 1));
-        dfs(j + 1);
-        part.pop();
+        curr.push(s.slice(i, j + 1));
+        backtrack(j + 1);
+        curr.pop();
       }
     }
-  }
-  dfs(0);
-  return res;
     
+  }
+  
+  backtrack(0);
+  return res;
 };
 
 function isPalindrome(s, i, j) {
   while (i < j) {
     if (s[i] !== s[j]) {
-      return false;
+     return false; 
     }
     i++;
     j--;
   }
-  
   return true;
 }
