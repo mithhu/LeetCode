5e1f0560f35ef6b64c6a80c5749ad3a503a43c91
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-24 21:54:55 +0600

    Time: 382 ms (31.58%), Space: 63.3 MB (24.09%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.js
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.js
@@ -3,22 +3,43 @@
  * @param {number} k
  * @return {number[][]}
  */
+// var kClosest = function(points, k) {
+//   let maxHeap = new MaxPriorityQueue({priority: (elem) => elem[0]});
+  
+//   for (let i = 0; i < k; i++) {
+//     let [x, y] = points[i];
+//     let dist = (x * x) + (y * y)
+//     maxHeap.enqueue([dist, [x, y]]);
+//   }
+  
+//   for (let i = k; i < points.length; i++) {
+//     let [x, y] = points[i];
+//     let dist = (x * x) + (y * y);
+    
+//     if (dist < maxHeap.front().element[0]) {
+//       maxHeap.dequeue();
+//       maxHeap.enqueue([dist, [x, y]]);
+//     }
+//   }
+  
+//   let res = [];
+    
+//   for (let elem of maxHeap.toArray()) {
+//     res.push(elem.element[1])
+//   }
+  
+//   return res;
+// };
+
 var kClosest = function(points, k) {
   let maxHeap = new MaxPriorityQueue({priority: (elem) => elem[0]});
   
-  for (let i = 0; i < k; i++) {
+  for (let i = 0; i < points.length; i++) {
     let [x, y] = points[i];
     let dist = (x * x) + (y * y)
     maxHeap.enqueue([dist, [x, y]]);
-  }
-  
-  for (let i = k; i < points.length; i++) {
-    let [x, y] = points[i];
-    let dist = (x * x) + (y * y);
-    
-    if (dist < maxHeap.front().element[0]) {
+    if (maxHeap.size() > k) {
       maxHeap.dequeue();
-      maxHeap.enqueue([dist, [x, y]]);
     }
   }
   
