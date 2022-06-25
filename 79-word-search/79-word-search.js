8aca29f05e6b0d6002ea8a7aa49daae60fd098f2
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-26 00:59:29 +0600

    Time: 382 ms (94.55%), Space: 42.2 MB (91.97%) - LeetHub

--- 79-word-search/79-word-search.js
+++ 79-word-search/79-word-search.js
@@ -4,30 +4,35 @@
  * @return {boolean}
  */
 var exist = function(board, word) {
-  function wordTraverse(i, j, idx) {
+  let R = board.length;
+  let C = board[0].length;
+  let visited = new Set();
+  
+  function dfs(r, c, idx) {
     if (idx === word.length) {
       return true;
     }
-    
-    if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || word[idx] !== board[i][j]) {
+    if (r < 0 || c < 0 || r === R || c === C || board[r][c] !== word[idx]) {
       return false;
     }
     
-    let temp = board[i][j];
-    board[i][j] = "#";
-    let res = wordTraverse(i + 1, j, idx + 1) || wordTraverse(i - 1, j, idx + 1) || wordTraverse(i, j + 1, idx + 1) || wordTraverse(i, j - 1, idx + 1);
-    board[i][j] = temp;
+    let temp = board[r][c];
+    board[r][c] = "#";
+    
+    let res = dfs(r + 1, c, idx + 1) ||  dfs(r - 1, c, idx + 1) || dfs(r, c + 1, idx + 1) ||     dfs(r, c - 1, idx + 1); 
+    
+    board[r][c] = temp;
     return res;
   }
-  
-  for (let r = 0; r < board.length; r++) {
-    for (let c = 0; c < board[0].length; c++) {
-      if (board[r][c] === word[0] && wordTraverse(r, c, 0)) {
-        return true;
+
+  for (let r = 0; r < R; r++) {
+    for (let c = 0; c < C; c++) {
+      if (word[0] === board[r][c]) {
+        if (dfs(r, c, 0)) {
+          return true;
+        }
       }
     }
   }
-  
   return false;
-    
 };
