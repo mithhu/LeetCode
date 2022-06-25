84d64fae2dad34e8352959cb18858f745b89884b
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-25 23:42:56 +0600

    Time: 102 ms (68.48%), Space: 51.4 MB (78.89%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.js
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.js
@@ -11,21 +11,20 @@
  * @return {number}
  */
 var maxPathSum = function(root) {
-  let maxSum = -Infinity;
+  let max = -Infinity;
   
   function dfs(node) {
     if (node === null) {
       return 0;
     }
-
-    let leftH = Math.max(dfs(node.left), 0);
-    let rightH = Math.max(dfs(node.right), 0);
-    maxSum = Math.max(maxSum, node.val + leftH + rightH);
-    return node.val + Math.max(leftH, rightH);  
+    let leftH = Math.max(0, dfs(node.left));
+    let rightH = Math.max(0, dfs(node.right));
+    max = Math.max(max, leftH + rightH + node.val);
+    return node.val + Math.max(leftH, rightH);
+    
   }
   
   dfs(root);
-  return maxSum;
-  
-  
+  return max;
+    
 };
