dad878ec3b7e00c2e5f8d13cb85560ee8aaa2f43
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-25 21:06:11 +0600

    Time: 289 ms (5.18%), Space: 50.9 MB (5.13%) - LeetHub

--- 57-insert-interval/57-insert-interval.js
+++ 57-insert-interval/57-insert-interval.js
@@ -7,25 +7,19 @@ var insert = function(intervals, newInterval) {
   let res = [];
   
   for (let i = 0; i < intervals.length; i++) {
-    let interval = intervals[i]
+    let interval = intervals[i];
     
-    //newinterval should go before and eturn
     if (newInterval[1] < interval[0]) {
       res.push(newInterval, ...intervals.slice(i));
-      return res;;
+      return res;
     } else if (newInterval[0] > interval[1]) {
-    //interval should go before
-     res.push(interval); 
+      res.push(interval);
     } else {
-     //merge interval
-      // newInterval[1] >= interval[0] && newInterval[0] <= interval[1]
-      newInterval = [Math.min(interval[0], newInterval[0]), Math.max(interval[1], newInterval[1])]
+      newInterval = [Math.min(interval[0], newInterval[0]), Math.max(interval[1], newInterval[1])];
     }
+    console.log(newInterval)
   }
-  
   res.push(newInterval);
-  
-  
   return res;
-    
+  
 };
