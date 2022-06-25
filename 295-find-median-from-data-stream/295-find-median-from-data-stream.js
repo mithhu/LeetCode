a1ba065b0e52ced97d9d1890b80230e6cdb52e94
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-26 01:29:08 +0600

    Time: 842 ms (67.25%), Space: 92.9 MB (58.04%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.js
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.js
@@ -1,34 +1,30 @@
-
 class MedianFinder {
   constructor() {
     this.small = new MaxPriorityQueue();
     this.large = new MinPriorityQueue();
   }
-  
+
   addNum(num) {
-    if (this.small.isEmpty() || this.small.front().element >= num) {
+    if (this.small.size() === 0 || this.small.front().element >= num) {
       this.small.enqueue(num);
-    } else {
+    }  else {
       this.large.enqueue(num);
     }
-    
-    if (this.small.size() > this.large.size() + 1) {
-      let elem = this.small.dequeue().element;
-      this.large.enqueue(elem);
-    } else if (this.small.size() < this.large.size()) {
-      let elem = this.large.dequeue().element;
-      this.small.enqueue(elem);
+
+    //make it valid
+    if (this.small.size() - this.large.size() > 1) {
+      this.large.enqueue(this.small.dequeue().element);
+    } else if (this.large.size() > this.small.size()) {
+      this.small.enqueue(this.large.dequeue().element);
     }
   }
-  
+
   findMedian() {
-    if ((this.small.size() + this.large.size()) % 2) {
-      return this.small.front().element;
-    }
-    
-    return (this.small.front().element + this.large.front().element) / 2;
+     if ((this.small.size() + this.large.size()) % 2) {
+       return this.small.front().element;
+     }
+
+     return (this.small.front().element + this.large.front().element) / 2;
   }
 };
-
-
 
