c114a29c6e1b86aeb83320e1ea845d31f7933362
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-25 20:07:46 +0600

    Time: 98 ms (33.07%), Space: 42.1 MB (70.73%) - LeetHub

--- 62-unique-paths/62-unique-paths.js
+++ 62-unique-paths/62-unique-paths.js
@@ -1,16 +1,22 @@
-/**
- * @param {number} m
- * @param {number} n
- * @return {number}
- */
 var uniquePaths = function(m, n) {
-  let grid = Array(n).fill(1);
+  let grid = Array(m).fill().map(()=> Array(n).fill(0));
+  
+  for (let c = 0; c < n; c++) {
+    grid[0][c] = 1; 
+  }
+  
+  for (let r = 0; r < m; r++) {
+    grid[r][0] = 1; 
+  }
     
   for (let r = 1; r < m; r++) {
     for (let c = 1; c < n; c++) {
-      grid[c] += grid[c - 1];
+      grid[r][c] = grid[r - 1][c] + grid[r][c - 1];
     }
   }
+ 
+  return grid.at(-1).at(-1); 
 
-  return grid[n - 1];
+
 };
+
