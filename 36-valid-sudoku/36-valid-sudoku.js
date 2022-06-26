6c242a06c63e80a9a20b37d103861baa57dc400e
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-26 22:01:01 +0600

    Time: 108 ms (59.09%), Space: 49.5 MB (10.02%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.js
+++ 36-valid-sudoku/36-valid-sudoku.js
@@ -1,40 +1,36 @@
-/**
- * @param {character[][]} board
- * @return {boolean}
- */
+
 var isValidSudoku = function(board) {
   let rows = {};
   let cols = {};
   let squares = {};
+  let R = board.length;
+  let C = board.length;
   
   for (let i = 0; i < board.length; i++) {
     rows[i] = new Set();
     cols[i] = new Set();
   }
-  
-  for (let r = 0; r < 9; r++) {
-    for (let c = 0; c < 9; c++) {
-      squares[`${Math.floor(r / 3)}*${Math.floor(c / 3)}`] = new Set();
+ 
+  for (let r = 0; r < R; r++) {
+    for (let c = 0; c < C; c++) {
+      squares[`${Math.floor(r / 3)}_${Math.floor(c / 3)}`] = new Set();
     }
   }
-  
-  for (let r = 0; r < 9; r++) {
-    for (let c = 0; c < 9; c++) {
+
+  for (let r = 0; r < R; r++) {
+    for (let c = 0; c < C; c++) {
       if (board[r][c] === ".") {
         continue;
-      } 
-      if (rows[r].has(board[r][c]) ||
-         cols[c].has(board[r][c]) ||
-         squares[`${Math.floor(r / 3)}*${Math.floor(c / 3)}`].has(board[r][c])) {
+      }
+      if (rows[r].has(board[r][c]) || cols[c].has(board[r][c]) || squares[`${Math.floor(r / 3)}_${Math.floor(c / 3)}`].has(board[r][c])) {
         return false;
       } else {
-        rows[r].add(board[r][c]);
-         cols[c].add(board[r][c]);
-         squares[`${Math.floor(r / 3)}*${Math.floor(c / 3)}`].add(board[r][c]);
+        rows[r].add(board[r][c]); 
+        cols[c].add(board[r][c]);
+        squares[`${Math.floor(r / 3)}_${Math.floor(c / 3)}`].add(board[r][c]);
       }
     }
   }
   
   return true;
-    
-};
+}
