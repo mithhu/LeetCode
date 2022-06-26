eb6dee96730c69f01b075f29e1cf6e9a35f01187
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-26 23:47:17 +0600

    Time: 99 ms (19.70%), Space: 42 MB (44.67%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.js
+++ 54-spiral-matrix/54-spiral-matrix.js
@@ -1,40 +1,51 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+
+
+
 var spiralOrder = function(matrix) {
-  let res = [];
   let left = 0;
   let right = matrix[0].length - 1;
   let top = 0;
   let bottom = matrix.length - 1;
-
+  let res = [];
+  
   while (left <= right && top <= bottom) {
-    //left to right
+    //top left to top right
     for (let i = left; i <= right; i++) {
-      res.push(matrix[top][i]);
+      res.push(matrix[top][i])
     }
     top++;
 
-    //top to bottom
+    //top right to bottom right
     for (let i = top; i <= bottom; i++) {
       res.push(matrix[i][right]);
     }
     right--;
+    
+    if (!(left <= right && top <= bottom)) {
+      break;
+    }
+   
+    
+    //bottom right to bottom left
+    for (let i = right; i >= left; i--) {
+      res.push(matrix[bottom][i])
+    }
+    bottom--;
 
-   if (!(left <= right && top <= bottom)) {
-     break;
-   }
-
-   //right to left
-   for (let i = right; i >= left; i--) {
-     res.push(matrix[bottom][i])
-   }
-   bottom--;
-
-   //bottom to top
-   for (let i = bottom; i >= top; i--) {
-     res.push(matrix[i][left]);
-   }
-   left++;
+    //bottom left to top left
+    for (let i = bottom; i >= top; i--) {
+      res.push(matrix[i][left]);
+    }
+    left++;
   }
 
   return res;
+  
+  
+    
+};
 
-}
