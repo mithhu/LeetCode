4cdc2b18c4d84fcbb299308c38508d998619e10a
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-27 00:35:34 +0600

    Time: 1509 ms (52.36%), Space: 49.7 MB (49.37%) - LeetHub

--- 212-word-search-ii/212-word-search-ii.js
+++ 212-word-search-ii/212-word-search-ii.js
@@ -1,62 +1,62 @@
-
 class TrieNode {
   constructor() {
     this.childrens = {};
-    this.endw = false;
+    this.word = false;
   }
 
   addWord(word) {
     let curr = this;
     for (let ch of word) {
-      if (!(ch in curr.childrens)) {
-        curr.childrens[ch] = new TrieNode();
-      }
-      curr = curr.childrens[ch];
+     if (!(ch in curr.childrens)) {
+       curr.childrens[ch] = new TrieNode()
+     }
+     curr = curr.childrens[ch];
     }
-    curr.endw = true;
+    curr.word = true;
   }
 }
+
 
 
 var findWords = function(board, words) {
   let root = new TrieNode();
-  for (let word of words) {
-    root.addWord(word);
-  }
+  words.forEach((word) => root.addWord(word));
 
   let R = board.length;
   let C = board[0].length;
   let res = [];
-
-  function wordFindHelper(r, c, node, currStr) {
-    if (r < 0 || c < 0 || r === R || c === C || !board[r][c] || !(board[r][c] in node.childrens)) {
-      return false;
-    }
-    currStr += board[r][c];
-    node = node.childrens[board[r][c]];
-    if (node.endw) {
-      res.push(currStr);
-      node.endw = false;
-    }
-    let temp = board[r][c];
-    board[r][c] = null;
-    wordFindHelper(r + 1, c, node, currStr);
-    wordFindHelper(r - 1, c, node, currStr);
-    wordFindHelper(r, c + 1, node, currStr);
-    wordFindHelper(r, c - 1, node, currStr);
-    board[r][c] = temp; 
-  }
-
   
+  function dfs(r, c, node, currStr) {
+    if (r < 0 || c < 0 || c === C || r === R || !board[r][c] || !(board[r][c] in node.childrens)) {
+     return false;
+   }
+   currStr += board[r][c];
+   node = node.childrens[board[r][c]];
+   if (node.word) {
+     res.push(currStr);
+     node.word = false;
+   }
+   let temp = board[r][c];
+   board[r][c] = null;
+
+   dfs(r + 1, c, node, currStr);
+   dfs(r - 1, c, node, currStr);
+   dfs(r, c + 1, node, currStr);
+   dfs(r, c - 1, node, currStr);
+   board[r][c] = temp;
+  }
 
   for (let r = 0; r < R; r++) {
     for (let c = 0; c < C; c++) {
-      wordFindHelper(r, c, root,  "");
+      dfs(r, c, root, "");
     }
   }
+ 
 
   return res;
+
   
+    
+};
 
 
-}
