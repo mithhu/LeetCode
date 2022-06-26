6c8115b5dfcedb278557ea75f8c321dea4fd01af
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-26 16:26:19 +0600

    Time: 124 ms (43.33%), Space: 45.2 MB (26.72%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.js
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.js
@@ -3,44 +3,41 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var setZeroes = function(matrix) {
-  let rowZero = false;
-  let ROWS = matrix.length;
-  let COLS = matrix[0].length;
-  
-  for (let r = 0; r < ROWS; r++) {
-    for (let c = 0; c < COLS; c++) {
+  let R = matrix.length;
+  let C = matrix[0].length;
+  let topRow = false;
+
+  for (let r = 0; r < R; r++) {
+    for (let c = 0; c < C; c++) {
       if (matrix[r][c] === 0) {
         matrix[0][c] = 0;
+        
         if (r > 0) {
           matrix[r][0] = 0;
         } else {
-          rowZero = true;
+          topRow = true;
         }
       }
-    }
+    } 
   }
-  
-  
-  for (let r = 1; r < ROWS; r++) {
-    for (let c = 1; c < COLS; c++) {
-      if (matrix[0][c] === 0 || matrix[r][0] === 0) {
+ 
+  for (let r = 1; r < R; r++) {
+    for (let c = 1; c < C; c++) {
+      if (matrix[r][0] === 0 || matrix[0][c] === 0) {
         matrix[r][c] = 0;
       }
     }
   }
-  
+
   if (matrix[0][0] === 0) {
-    for (let r = 0; r < ROWS; r++) {
+    for (let r = 0; r < R; r++) {
       matrix[r][0] = 0;
     }
-  }
-  
-  if (rowZero) {
-    for (let c = 0; c < COLS; c++) {
+  } 
+
+  if (topRow) {
+    for (let c = 0; c < C; c++) {
       matrix[0][c] = 0;
     }
   }
-  
-  
-    
 };
