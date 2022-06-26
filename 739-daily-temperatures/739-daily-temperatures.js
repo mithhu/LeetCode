2abaec125d194cc75aa612388121c13ff43365a2
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-26 22:56:16 +0600

    Time: 528 ms (30.81%), Space: 71.1 MB (25.59%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.js
+++ 739-daily-temperatures/739-daily-temperatures.js
@@ -5,16 +5,17 @@
 var dailyTemperatures = function(temperatures) {
   let res = Array(temperatures.length).fill(0);
   let stack = [];
-  
+
   for (let i = 0; i < temperatures.length; i++) {
     let t = temperatures[i];
-    while (stack.length && stack.at(-1)[0] < t) {
-      let [sT, sI] = stack.pop();
-      res[sI] = i - sI;
+    while (stack.length && t > stack.at(-1)[0]) {
+      let [sT, sIdx] = stack.pop();
+      res[sIdx] = i - sIdx;
     }
-    
+
     stack.push([t, i]);
   }
-  
+
   return res;
-};
+  
+}
