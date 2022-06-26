89dfa3cd28c6d124e16ed6ebb3df32fa7a8e6655
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-06-26 21:25:20 +0600

    Time: 115 ms (31.54%), Space: 43.5 MB (40.16%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.js
+++ 42-trapping-rain-water/42-trapping-rain-water.js
@@ -5,23 +5,21 @@
 var trap = function(height) {
   let l = 0;
   let r = height.length - 1;
-  let res = 0;
   let leftMax = height[l];
-  let rightMax = height[r];
-  
+  let rightMax = height.at(-1);
+  let res = 0;
+
   while (l < r) {
     if (leftMax < rightMax) {
       l++;
       res += leftMax - height[l] > 0 ? leftMax - height[l] : 0;
-      leftMax= Math.max(leftMax, height[l]);
+      leftMax = Math.max(leftMax, height[l]);
     } else {
       r--;
       res += rightMax - height[r] > 0 ? rightMax - height[r] : 0;
-      rightMax= Math.max(rightMax, height[r]);
+      rightMax = Math.max(rightMax, height[r]);
     }
   }
-  
   return res;
-  
-    
-};
+
+}
