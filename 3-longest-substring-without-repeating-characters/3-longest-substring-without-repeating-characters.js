007f5ca901b23db3c8095508713ab91ea58a070b
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-07-24 02:42:39 +0600

    Time: 82 ms (97.66%), Space: 46.2 MB (82.80%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -4,20 +4,19 @@
  */
 var lengthOfLongestSubstring = function(s) {
   let set = new Set();
-  
   let l = 0;
   let r = 0;
   let res = 0;
   
   while (r < s.length) {
-    while(set.has(s[r])) {
+    while (set.has(s[r])) {
       set.delete(s[l]);
       l++;
     }
-    
     set.add(s[r]);
     res = Math.max(r - l + 1, res);
     r++;
   }
   return res;
+  
 };
