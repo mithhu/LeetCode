7c978cf320cb25e7031ef5616b8f19be9801c651
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-07-24 02:48:10 +0600

    Time: 661 ms (80.36%), Space: 96 MB (85.03%) - LeetHub

--- 146-lru-cache/146-lru-cache.js
+++ 146-lru-cache/146-lru-cache.js
@@ -1,65 +1,25 @@
-/**
- * @param {number} capacity
- */
-
-class Node {
-  constructor(key, val) {
-    this.key = key;
-    this.val = val;
-    this.prev = null;
-    this.next = null;
-  }
-}
-
 class LRUCache {
-  // cap, left, r, cache
-  constructor(cap) {
-    this.cap = cap;
+  constructor(capacity) {
     this.cache = new Map();
-    this.left = new Node(0, 0);
-    this.right = new Node(0, 0);
-    this.left.next = this.right;
-    this.right.prev = this.left;
+    this.capacity = capacity;
   }
-  
-  insert(node) {
-    let prev = this.right.prev;
-    let next = this.right;
-    prev.next = node;
-    next.prev = node;
-    node.next = next;
-    node.prev = prev;
-  }
-  
-  remove(node) {
-    let prev = node.prev;
-    let next = node.next;
-    prev.next = next;
-    next.prev = prev;
-  }
-  
+
   get(key) {
-    if (!(this.cache.has(key))) {
-      return -1;
-    };
-    this.remove(this.cache.get(key));
-    this.insert(this.cache.get(key));
-    return this.cache.get(key).val;
+    if (!this.cache.has(key)) return -1;
+
+    const v = this.cache.get(key);
+    this.cache.delete(key);
+    this.cache.set(key, v);
+    return this.cache.get(key);
   }
-  
-  put(key, val) {
+
+  put(key, value) {
     if (this.cache.has(key)) {
-      this.remove(this.cache.get(key));
-    };
-    this.cache.set(key, new Node(key, val));
-    this.insert(this.cache.get(key));
-    
-    if (this.cache.size > this.cap) {
-      let lru = this.left.next;
-      this.remove(lru);
-      this.cache.delete(lru.key);
+      this.cache.delete(key);
     }
-    
+    this.cache.set(key, value);
+    if (this.cache.size > this.capacity) {
+      this.cache.delete(this.cache.keys().next().value); // keys().next().value returns first item's key
+    }
   }
 }
-
