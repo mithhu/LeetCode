78f0006bc3bd51fd43cccbb338e7044f9fda912c
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-08-19 21:41:29 +0600

    Time: 60 ms (94.41%), Space: 42.1 MB (26.77%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.js
+++ 371-sum-of-two-integers/371-sum-of-two-integers.js
@@ -4,12 +4,11 @@
  * @return {number}
  */
 var getSum = function(a, b) {
-  while ( b !== 0) {
-    let temp = (a & b) << 1;
-    a = a ^ b;
-    b = temp;
-  }
-  
+ while (b !== 0) {
+   let temp = (a & b) << 1;
+   a = a ^ b;
+   b = temp;
+ }
   return a;
     
 };
