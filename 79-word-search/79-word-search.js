2aedf6a3c1360c307de183b84083d426d1be5cbd
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-08-19 21:17:14 +0600

    Time: 430 ms (90.94%), Space: 42.4 MB (89.13%) - LeetHub

--- 79-word-search/79-word-search.js
+++ 79-word-search/79-word-search.js
@@ -4,35 +4,39 @@
  * @return {boolean}
  */
 var exist = function(board, word) {
-  let R = board.length;
-  let C = board[0].length;
-  let visited = new Set();
+  const R = board.length;
+  const C = board[0].length;
+  
+
   
   function dfs(r, c, idx) {
     if (idx === word.length) {
       return true;
     }
-    if (r < 0 || c < 0 || r === R || c === C || board[r][c] !== word[idx]) {
+    
+    if (r < 0 || c < 0 || r >= R || c >= C || board[r][c] !== word[idx]) {
       return false;
     }
     
+   
+    
     let temp = board[r][c];
     board[r][c] = "#";
-    
-    let res = dfs(r + 1, c, idx + 1) ||  dfs(r - 1, c, idx + 1) || dfs(r, c + 1, idx + 1) ||     dfs(r, c - 1, idx + 1); 
-    
+    let res = dfs(r + 1, c, idx + 1) || dfs(r - 1, c, idx + 1) || dfs(r, c + 1, idx + 1) || dfs(r, c - 1, idx + 1);
     board[r][c] = temp;
     return res;
   }
-
+  
   for (let r = 0; r < R; r++) {
     for (let c = 0; c < C; c++) {
-      if (word[0] === board[r][c]) {
+      if (board[r][c] === word[0]) {
         if (dfs(r, c, 0)) {
           return true;
         }
       }
     }
   }
+  
   return false;
+    
 };
