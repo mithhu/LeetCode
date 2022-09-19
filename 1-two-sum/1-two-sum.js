d2cfb06a622b4e42fc60058a4a3129b44a66a2a4
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-09-19 21:15:16 +0600

    Time: 94 ms (79.95%), Space: 43.1 MB (35.09%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -7,15 +7,12 @@ var twoSum = function(nums, target) {
   let map = {};
   
   for (let i = 0; i < nums.length; i++) {
-    let num = nums[i];
-    const required = target - num;
-    if (required in map) {
-      return [map[required], i]
-    } else {
-      map[num] = i;
+    const desiredVal = target - nums[i];
+    
+    if (desiredVal in map) {
+      return [i, map[desiredVal]];
     }
+    map[nums[i]] = i;
   }
-  
-  return -1;
     
 };
