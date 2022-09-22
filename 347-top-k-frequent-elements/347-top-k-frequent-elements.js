196723bbe602df37cfa7f107cdbd77e7aac8dc52
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-09-23 01:17:32 +0600

    Time: 173 ms (11.63%), Space: 48.4 MB (18.40%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.js
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.js
@@ -4,28 +4,33 @@
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) {
-  let count = {};
+  let maxFreq = 0; 
+  
+  const count = {};
   for (let num of nums) {
     if (!(num in count)) {
-      count[num] = 0; 
+      count[num] = 0;
     }
     count[num]++;
-  }
-  
-  let freq = Array(nums.length + 1).fill().map(() => []);
-  for (let key in count) {
-    freq[count[key]].push(key);
-  }
-  console.log(freq)
-  
-  let res = [];
-  for (let i = freq.length - 1; i >= 0; i--) {
-    for (let num of freq[i]) {
-      res.push(num);
-      if (res.length === k) {
-        return res;
-      }
+    if (count[num] > maxFreq) {
+      maxFreq = count[num];  
     }
   }
-    
+  console.log(count);
+  
+  
+  const freqIdx = Array(maxFreq + 1).fill().map(() => []);
+  
+  for (let key in count) {
+    freqIdx[count[key]].push(key);
+  }
+  
+  const res = [];
+  
+  for (let i = freqIdx.length - 1; i >= 0; i--) {
+    freqIdx[i].forEach((key) => res.push(key));
+    if (res.length === k) {
+      return res;
+    }
+  }
 };
