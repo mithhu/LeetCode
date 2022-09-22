d23f893fec7291b84769f500708472467f7bad17
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-09-23 01:45:11 +0600

    Time: 201 ms (23.60%), Space: 53.3 MB (83.86%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.js
+++ 238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -3,22 +3,20 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-  let prefix = 1;
+  const res = Array(nums.length).fill();
   
-  let res = Array(nums.length).fill(1);
+  let prefix = 1;
   for (let i = 0; i < nums.length; i++) {
     res[i] = prefix;
     prefix *= nums[i];
   }
-
+  
   let postfix = 1;
   for (let i = nums.length - 1; i >= 0; i--) {
     res[i] *= postfix;
     postfix *= nums[i];
   }
-
-  return res;
   
+  return res;
     
 };
-
