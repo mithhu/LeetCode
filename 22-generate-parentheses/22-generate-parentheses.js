200d55ca7d3023158c23955916fa0d4a93e69459
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-09-28 03:18:10 +0600

    Time: 104 ms (38.10%), Space: 42.7 MB (58.43%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.js
+++ 22-generate-parentheses/22-generate-parentheses.js
@@ -2,30 +2,30 @@
  * @param {number} n
  * @return {string[]}
  */
-
-function generateParenthesis(num) {
-  let stack = [];
-  let res = [];
+var generateParenthesis = function(n) {
+  const res = [];
+  const stack = [];
   
-  function backTrack(open, close) {
-    if (open === num && close === num) {
+  function backtrack(open, close) {
+    if (open === n && close === n) {
       res.push(stack.join(""));
       return;
     }
     
-    if (open < num) {
+    if (open < n) {
       stack.push("(");
-      backTrack(open + 1, close)
-      stack.pop("(");
+      backtrack(open + 1, close);
+      stack.pop();
     }
     
     if (close < open) {
       stack.push(")");
-      backTrack(open, close + 1)
-      stack.pop(")");
+      backtrack(open, close + 1);
+      stack.pop();
     }
   }
   
-  backTrack(0, 0);
+  backtrack(0, 0);
   return res;
-}
+    
+};
