ef1b0d301c57539c9288dfb98afb319ffbef946d
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-10-04 13:27:28 +0600

    Time: 146 ms (43.25%), Space: 56 MB (41.91%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.js
+++ 41-first-missing-positive/41-first-missing-positive.js
@@ -3,15 +3,14 @@
  * @return {number}
  */
 var firstMissingPositive = function(nums) {
-  let set = new Set();
-  for (let i = 0; i < nums.length; i++) {
-      set.add(nums[i]);
-  }
-  for (let i = 1; i < nums.length + 1; i++) {
-      if (!set.has(i)) {
-        return i;
-      }
-  }
-  return nums.length + 1;
+  const set = new Set(nums);
   
-}
+  for (let i = 1; i < nums.length + 1; i++) {
+    if (!(set.has(i))) {
+      return i;
+    }
+  }
+  
+  return nums.length + 1;
+    
+};
