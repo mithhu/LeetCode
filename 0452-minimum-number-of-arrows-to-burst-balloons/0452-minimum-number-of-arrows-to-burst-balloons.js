8bd0aff42f19f8ad2cc5fb695a45fca78481841b
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-11-19 02:36:57 +0800

    Time: 456 ms (53.52%), Space: 75.2 MB (33.51%) - LeetHub

---
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var findMinArrowShots = function(points) {
+    points.sort((a, b) => a[1] - b[1])
+    let count = 1;
+    let end = points[0][1];
+    
+    for (let i = 1; i < points.length; i++) {
+        if (points[i][0] > end) {
+            count++;
+            end = points[i][1];     
+        }
+    }
+    
+    
+    return count;
+    
+};
