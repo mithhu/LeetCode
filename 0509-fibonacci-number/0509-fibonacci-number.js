b05e27e4821f4ea741324d51167fc480235d8970
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-11-19 01:23:00 +0800

    Time: 1397 ms (5.02%), Space: 48.5 MB (5.02%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -2,10 +2,16 @@
  * @param {number} n
  * @return {number}
  */
-var fib = function(n) {
+var fib = function(n, map = {}) {
     if (n === 0 || n === 1) {
         return n;
     }
-    return fib(n - 1) + fib(n - 2);
+    
+    if (n in map) {
+        return map[n];
+    }
+    
+    map[n] = fib(n - 1) + fib(n - 2);
+    return map[n];
     
 };
