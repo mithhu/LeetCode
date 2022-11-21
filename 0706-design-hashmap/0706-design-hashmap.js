a8b7f87883c4ba434034bc446920b725b06e9e96
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-11-22 01:25:56 +0800

    Time: 239 ms (83.10%), Space: 54 MB (26.52%) - LeetHub

---
+++ 0706-design-hashmap/0706-design-hashmap.js
@@ -0,0 +1,39 @@
+class ListNode {
+    constructor(key, val, next) {
+        this.key = key
+        this.val = val
+        this.next = next
+    }
+}
+class MyHashMap {
+    constructor() {
+        this.size = 19997
+        this.mult = 12582917
+        this.data = new Array(this.size)
+    }
+    hash(key) {
+        return key * this.mult % this.size
+    }
+    put(key, val) {
+        this.remove(key)
+        let h = this.hash(key)
+        let node = new ListNode(key, val, this.data[h])
+        this.data[h] = node
+    }
+    get(key) {
+        let h = this.hash(key), node = this.data[h]
+        for (; node; node = node.next)
+            if (node.key === key) return node.val
+        return -1
+    }
+    remove(key) {
+        let h = this.hash(key), node = this.data[h]
+        if (!node) return
+        if (node.key === key) this.data[h] = node.next
+        else for (; node.next; node = node.next)
+            if (node.next.key === key) {
+                node.next = node.next.next
+                return
+            }
+    }
+};
