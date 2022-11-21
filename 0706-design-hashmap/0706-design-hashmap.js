02afb21bf1fc71fd878e3ce39c06db18fa53bc51
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-11-22 01:51:15 +0800

    Time: 265 ms (79.37%), Space: 52.8 MB (32.42%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.js
+++ 0706-design-hashmap/0706-design-hashmap.js
@@ -1,46 +1,49 @@
 class ListNode {
-    constructor(key, val, next) {
-        this.key = key
-        this.val = val
-        this.next = next
-    }
+  constructor(key, val, next) {
+    this.key = key;
+    this.val = val;
+    this.next = next;
+  }
 }
+
 class MyHashMap {
-    constructor() {
-        this.size = 769
-        this.data = new Array(this.size)
+  constructor() {
+    this.size = 769;
+    this.data = new Array(this.size);
+  }
+  hash(key) {
+    return key % this.size;
+  }
+  put(key, val) {
+    this.remove(key);
+    let h = this.hash(key);
+    let node = new ListNode(key, val, this.data[h]);
+    this.data[h] = node;
+  }
+  get(key) {
+    let h = this.hash(key),
+      node = this.data[h];
+    while (node) {
+      if (node.key === key) return node.val;
+      node = node.next;
     }
-    hash(key) {
-        return key % this.size
-    }
-    put(key, val) {
-        this.remove(key)
-        let h = this.hash(key)
-        let node = new ListNode(key, val, this.data[h])
-        this.data[h] = node
-    }
-    get(key) {
-        let h = this.hash(key), node = this.data[h]
-        while (node) {
-            if (node.key === key) return node.val
-            node = node.next;
-        }
-            
-        return -1
-    }
-      remove(key) {
+
+    return -1;
+  }
+  remove(key) {
     let h = this.hash(key),
       node = this.data[h];
     if (!node) return;
-    if (node.key === key) this.data[h] = node.next;
-    else {
-      while (node.next) {
-        if (node.next.key === key) {
-          node.next = node.next.next;
-          return;
-        }
-        node = node.next;
+    if (node.key === key) {
+      this.data[h] = node.next;
+      return;
+    }
+    while (node.next) {
+      if (node.next.key === key) {
+        node.next = node.next.next;
+        return;
       }
+      node = node.next;
     }
   }
-};
+}
