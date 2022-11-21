6807c92de76ce7903c3536bd3fdbcfaf471dbab1
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2022-11-22 03:47:44 +0800

    Time: 392 ms (82.52%), Space: 58.1 MB (94.42%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.js
+++ 0912-sort-an-array/0912-sort-an-array.js
@@ -3,46 +3,44 @@
  * @return {number[]}
  */
 var sortArray = function(nums) {
-    quickSortHelper(nums, 0, nums.length - 1);
+    buildMaxHeap(nums);
+    for (let endI = nums.length - 1; endI > 0; endI--) {
+        swap(0, endI, nums);
+        siftDown(0, endI - 1, nums);
+    }
     return nums;
-    
 };
 
-function quickSortHelper(nums, start, end) {
-    if (start >= end) {
-        return;
-    }
-    
-    let p = start;
-    let l = start + 1;
-    let r = end;
-    
-    while (l <= r) {
-        if (nums[l] > nums[p] && nums[r] < nums[p]) {
-            swap(nums, l, r);
-            l++;
-            r--;
-        } 
-        if (nums[l] <= nums[p]) {
-            l++;
-        }
-        if (nums[r] >= nums[p]) {
-            r--
-        }
-    }
-    swap(nums, r, p);
-    let leftArrSmall = r - 1 - start < end - r + 1;
-    if (leftArrSmall) {
-        quickSortHelper(nums, start, r - 1);
-        quickSortHelper(nums, r + 1, end);
-    } else {
-        quickSortHelper(nums, r + 1, end);
-        quickSortHelper(nums, start, r - 1);
+function buildMaxHeap(arr) {
+    let parentI = Math.floor((arr.length - 2) / 2);
+    for (let curr = parentI; curr >= 0; curr--) {
+        siftDown(curr, arr.length - 1, arr);
     }
 }
 
-function swap(arr, l, r) {
-    let temp = arr[l];
-    arr[l] = arr[r];
-    arr[r] = temp;
+function siftDown(curr, e, arr) {
+    let ch1 = curr * 2 + 1;
+    while (ch1 <= e) {
+        let ch2 = curr * 2 + 2 <= e ?  curr * 2 + 2 : -1;
+        let idxSwap;
+        if (ch2 !== -1 && arr[ch2] > arr[ch1]) {
+            idxSwap = ch2;
+        } else {
+            idxSwap = ch1;
+        }
+        if (arr[idxSwap] > arr[curr]) {
+            swap(idxSwap, curr, arr);
+            curr = idxSwap;
+            ch1 = curr * 2 + 1;
+        } else {
+            return;
+        }
+    }
 }
+
+function swap(i, j, arr) {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
