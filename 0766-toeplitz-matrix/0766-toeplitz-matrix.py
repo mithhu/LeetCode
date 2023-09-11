5bd7b0be3a226884e4961bc064f456e18e594156
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2023-09-12 02:56:13 +0800

    Time: 87 ms (58.63%), Space: 16.3 MB (85.75%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.py
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.py
@@ -1,11 +1,12 @@
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
-        r = 0
-        c = 0
+        # r = 0
+        # c = 0
         R = len(matrix)
         C = len(matrix[0])
         
-        while r < R and c < C:
+        r = 0
+        for c in range(C):
             val = matrix[r][c]
             i, j = r, c
             
@@ -14,11 +15,9 @@ class Solution:
                     return False
                 i += 1
                 j += 1
-            c += 1
         
-        r = 1
         c = 0
-        while r < R and c < C:
+        for r in range(1, R):
             val = matrix[r][c]
             i, j = r, c
             
@@ -27,7 +26,6 @@ class Solution:
                     return False
                 i += 1
                 j += 1
-            r += 1
         return True
             
         
