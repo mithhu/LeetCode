1ac1a6e2528c2ed6807a8d127afc12ef35127f4d
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2023-09-12 03:05:02 +0800

    Time: 79 ms (92.28%), Space: 16.2 MB (99.00%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.py
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.py
@@ -1,31 +1,25 @@
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
-        # r = 0
-        # c = 0
         R = len(matrix)
         C = len(matrix[0])
         
-        r = 0
+        def is_diagonal_univalue(r, c):
+            val = matrix[r][c]
+            while r < R and c < C:
+                if matrix[r][c] != val:
+                    return False
+                r += 1
+                c += 1
+            return True
+            
         for c in range(C):
-            val = matrix[r][c]
-            i, j = r, c
-            
-            while i < R and j < C:
-                if matrix[i][j] != val:
-                    return False
-                i += 1
-                j += 1
+            if not is_diagonal_univalue(0, c):
+                return False
         
-        c = 0
         for r in range(1, R):
-            val = matrix[r][c]
-            i, j = r, c
+            if not is_diagonal_univalue(r, 0):
+                return False
             
-            while i < R and j < C:
-                if matrix[i][j] != val:
-                    return False
-                i += 1
-                j += 1
         return True
             
         
