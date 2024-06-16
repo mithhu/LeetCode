b695da68ccffb7f8b2eff8376154c8d5a08ded1b
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2024-06-16 21:39:37 +0800

    Time: 55 ms (25.00%), Space: 49 MB (37.50%) - LeetHub

---
+++ 1432-max-difference-you-can-get-from-changing-an-integer/1432-max-difference-you-can-get-from-changing-an-integer.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var maxDiff = function(num) {
+    const numStr = num.toString();
+    
+    // Step to find the maximum possible number (a)
+    let maxNumStr = numStr;
+    for (let char of numStr) {
+        if (char !== '9') {
+            maxNumStr = numStr.replaceAll(char, '9');
+            break;
+        }
+    }
+    const maxNum = parseInt(maxNumStr, 10);
+
+    // Step to find the minimum possible number (b)
+    let minNumStr;
+    if (numStr[0] !== '1') {
+        minNumStr = numStr.replaceAll(numStr[0], '1');
+    } else {
+        minNumStr = numStr;
+        for (let i = 1; i < numStr.length; i++) {
+            if (numStr[i] !== '0' && numStr[i] !== '1') {
+                minNumStr = numStr.replaceAll(numStr[i], '0');
+                break;
+            }
+        }
+    }
+    const minNum = parseInt(minNumStr, 10);
+
+    // Calculate and return the difference
+    return maxNum - minNum;
+}
