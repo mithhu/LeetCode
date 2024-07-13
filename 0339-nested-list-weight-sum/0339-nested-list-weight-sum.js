77d628398f0fbddf1259bcaa90061d9c37ce3466
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2024-07-13 16:13:36 +0800

    Time: 61 ms (10.41%), Space: 49.5 MB (40.47%) - LeetHub

---
+++ 0339-nested-list-weight-sum/0339-nested-list-weight-sum.js
@@ -0,0 +1,63 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * function NestedInteger() {
+ *
+ *     Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     @return {boolean}
+ *     this.isInteger = function() {
+ *         ...
+ *     };
+ *
+ *     Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     Return null if this NestedInteger holds a nested list
+ *     @return {integer}
+ *     this.getInteger = function() {
+ *         ...
+ *     };
+ *
+ *     Set this NestedInteger to hold a single integer equal to value.
+ *     @return {void}
+ *     this.setInteger = function(value) {
+ *         ...
+ *     };
+ *
+ *     Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+ *     @return {void}
+ *     this.add = function(elem) {
+ *         ...
+ *     };
+ *
+ *     Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     Return null if this NestedInteger holds a single integer
+ *     @return {NestedInteger[]}
+ *     this.getList = function() {
+ *         ...
+ *     };
+ * };
+ */
+/**
+ * @param {NestedInteger[]} nestedList
+ * @return {number}
+ */
+var depthSum = function(nestedList) {
+    let depth = 1;
+    let sum = 0;
+    const q = [...nestedList];
+    
+    while (q.length > 0) {
+        const size = q.length;
+        for (let i = 0; i < size; i++) {
+            let curr = q.shift();
+            if (curr.isInteger()) {
+                sum += (curr.getInteger() * depth)
+            } else {
+                q.push(...curr.getList())
+            }
+        }
+        depth++;
+    }
+    
+    return sum;
+    
+};
