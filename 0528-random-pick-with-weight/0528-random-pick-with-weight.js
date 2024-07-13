481e6bcf9dcaf5524c61821e0da4cc9ea9b22bed
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2024-07-14 02:40:05 +0800

    Time: 141 ms (54.40%), Space: 60.8 MB (23.39%) - LeetHub

--- 0528-random-pick-with-weight/0528-random-pick-with-weight.js
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.js
@@ -18,8 +18,8 @@
  * var param_1 = obj.pickIndex()
  */
 
-class Solution {
-    constructor(w) {
+function Solution(w) {
+
         this.prefixSum = [];
         let prefixSum = 0;
         for (const weight of w) {
@@ -27,24 +27,24 @@ class Solution {
             this.prefixSum.push(prefixSum);
         }
         this.maxSum = prefixSum;
-    }
-    
-    pickIndex() {
-        const target = Math.random() * this.maxSum;
-        let l = 0;
-        let r = this.prefixSum.length - 1;
-        
-        while (l <= r) {
-            let mid = Math.floor((l + r) / 2);
-            if (this.prefixSum[mid] === target) {
-                return mid;
-            } else if (this.prefixSum[mid] < target) {
-                l = mid + 1;
-            } else {
-                r = mid - 1;
-            }
-        }
-        return l;
-    }
 }
+    
+Solution.prototype.pickIndex = function() {
+    const target = Math.random() * this.maxSum;
+    let l = 0;
+    let r = this.prefixSum.length - 1;
+
+    while (l <= r) {
+        let mid = Math.floor((l + r) / 2);
+        if (this.prefixSum[mid] === target) {
+            return mid;
+        } else if (this.prefixSum[mid] < target) {
+            l = mid + 1;
+        } else {
+            r = mid - 1;
+        }
+    }
+    return l;
+}
+
 
