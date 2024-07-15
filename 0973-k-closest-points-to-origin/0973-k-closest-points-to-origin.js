c773c9449038bbf9d4315ffcee329fb3d9fc96d9
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2024-07-16 01:05:32 +0800

    Time: 289 ms (16.24%), Space: 71.8 MB (10.62%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.js
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.js
@@ -6,20 +6,23 @@
 var kClosest = function(points, k) {
     let maxHeap = new MaxPriorityQueue({priority: (elem) => elem[0]});
     
-    for (let [i, j] of points) {
-        let dist = i * i + j * j;
-        maxHeap.enqueue([dist, [i, j]]);
-        
-        if (maxHeap.size() > k){
-            maxHeap.dequeue();
+    for (let [r, c] of points) {
+        let d = (r * r) + (c * c);
+        if (maxHeap.size() < k) {
+            maxHeap.enqueue([d, r, c ])
+        } else {
+            if (maxHeap.front().priority > d) {
+                maxHeap.dequeue();
+                maxHeap.enqueue([d, r, c])
+            }
         }
     }
-    
+
     let res = [];
-    for (let elem of maxHeap.toArray()) {
-        res.push(elem.element[1])
+    for (let {element: [d, r, c]} of maxHeap.toArray()) {
+        res.push([r, c])
     }
     
+    console.log(res);
     return res;
-    
 };
