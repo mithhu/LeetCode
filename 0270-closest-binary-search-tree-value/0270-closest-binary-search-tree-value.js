6f18666157c2486c4aa5a9b8dd8a1fe4bf7b34d5
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2024-07-18 02:59:25 +0800

    Time: 66 ms (28.02%), Space: 51 MB (98.85%) - LeetHub

--- 0270-closest-binary-search-tree-value/0270-closest-binary-search-tree-value.js
+++ 0270-closest-binary-search-tree-value/0270-closest-binary-search-tree-value.js
@@ -11,31 +11,22 @@
  * @param {number} target
  * @return {number}
  */
-
-
-
-   function closestValue(root, target) {
-        let closest = root.val;
-
-        while (root !== null) {
-            if (Math.abs(target - root.val) < Math.abs(target - closest) || 
-                (Math.abs(target - root.val) === Math.abs(target - closest) && root.val < closest)) {
-                closest = root.val;
-            }
-
-            if (target < root.val) {
-                root = root.left;
-            } else {
-                root = root.right;
-            }
+var closestValue = function(root, target) {
+    let closest = root.val;
+    
+    while (root !== null) {
+        if (Math.abs(root.val - target) < Math.abs(target - closest) || 
+            (Math.abs(root.val - target) === Math.abs(target - closest)
+             && root.val < closest)) {
+            closest = root.val;
         }
-
-        return closest;
+        if (target < root.val) {
+            root = root.left;
+        } else {
+            root = root.right;
+        }
     }
-
-
-// Example usage:
-// const root = new TreeNode(4, new TreeNode(2, new TreeNode(1), new TreeNode(3)), new TreeNode(5));
-// const solution = new Solution();
-// console.log(solution.closestValue(root, 3.714286)); // Output: 4
-
+    
+    return closest;
+    
+};
