585fbbee3063ee5defc98ee7623a011a82baa490
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2024-07-17 20:19:21 +0800

    Time: 46 ms (86.84%), Space: 48.9 MB (31.82%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.js
@@ -0,0 +1,44 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var maximumSwap = function(num) {
+    const numAsArr = [];
+    
+    while (num) {
+        numAsArr.unshift(num % 10);
+        num = Math.floor(num / 10)
+    }
+    
+    let maxVal = -1;
+    let maxIdx = numAsArr.length;
+    
+    let i = numAsArr.length - 1;
+    
+    while (i >= 0) {
+        let currVal = numAsArr[i];
+        numAsArr[i] = [currVal, maxVal, maxIdx];
+        if (currVal > maxVal) {
+            maxVal = currVal;
+            maxIdx = i;
+        }
+        i--;
+    }
+    
+    i = 0;
+    
+    while (i < numAsArr.length) {
+        let [currVal, maxToRight, maxRightIdx] = numAsArr[i];
+        if (currVal < maxToRight) {
+            [numAsArr[i], numAsArr[maxRightIdx]] = [numAsArr[maxRightIdx], numAsArr[i]];
+             break;
+        }
+        i++;
+    }
+    
+    let res = 0;
+    for (let [num, _, __] of numAsArr) {
+        res = (res * 10) + num; 
+    }
+    return res;
+};
